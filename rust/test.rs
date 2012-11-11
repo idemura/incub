cf5cb437f74e9964bc768d967708c51cea1c888a
Igor Demura <idemura@yandex.ru>
2012-11-11 09:01:55 -0800

    Rust test

--- rust/test.rs
+++ rust/test.rs
@@ -1,6 +1,22 @@
 // Rust programming test
 
+use core::*;
+use os::list_dir;
+
+fn collect_matches(dir: &str) -> ~[~str]
+{
+  let mut v = dvec::DVec();
+  v.get()
+}
+
 fn main()
 {
+  let fs = list_dir(&Path(&"."));
+
+  
+  for fs.each |f| {
+    io::println(fmt!("%s", *f));
+  }
+
   io::println("1234");
 }

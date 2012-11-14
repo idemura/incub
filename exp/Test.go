ae78995c3a8388871dc6603ae44f3bcedbcdbcdd
Igor Demura <idemura@yandex.ru>
2012-11-13 23:12:09 -0800

    Go playground.

--- exp/Test.go
+++ exp/Test.go
@@ -1,2 +1,15 @@
-// Hello
+package main
 
+import ("fmt"; "os"; "path/filepath")
+
+func main() {
+  const pat = "?ur*.hs"
+  visit := func (path string, fi os.FileInfo, err error) error {
+    if m, _ := filepath.Match(pat, path); m {
+      fmt.Printf("%s\n", path)
+    }
+    return nil
+  }
+
+  filepath.Walk(".", visit);
+}

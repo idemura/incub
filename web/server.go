f824af069c451fa6dc5cc44644c4076120da2459
Igor Demura <igord@google.com>
2012-11-14 10:32:50 -0800

    Toy server

--- web/server.go
+++ web/server.go
@@ -1,7 +1,20 @@
 package main
 
-import ("fmt")
+import ("fmt"; "net/http")
+
+type Server struct {
+  // State
+}
+
+func (h *Server) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+  fmt.Fprintf(w, "Hello!")
+}
 
 func main() {
-
+  const address = "localhost:4000"
+  fmt.Printf("Server started at %s\n", address)
+  var srv = new(Server)
+  http.ListenAndServe(address, srv)
 }

ea12ff8585c200d415076f75cd62e0cae868b203
Igor Demura <idemura@yandex.ru>
2012-11-14 22:35:44 -0800

    Server logs access

--- web/server.go
+++ web/server.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+  "io"
   "fmt"
   "net/http"
+  "log"
 )
 
 type Server struct {
   // State
 }
 
+func TrimTrailingSplases(s string) string {
+  l := len(s)
+  if l == 0 {
+    return s
+  }
+
+  for i := l - 1; i > 0; i-- {
+    if s[i] != '/' {
+      return s[: i + 1]
+    }
+  }
+  return ""
+}
+
 func (h *Server) ServeHTTP(
     w http.ResponseWriter,
     r *http.Request) {
-  fmt.Fprintf(w, "Hello!")
+  log.Printf("Request path: %s", r.URL.Path);
 }
 
 func main() {

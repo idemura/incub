4754f51f4a4e478d3f4ea3697bac3c849d5eed5f
Igor Demura <igord@google.com>
2012-12-14 17:50:20 -0800

    Model data for user

--- web/src/server/model.go
+++ web/src/server/model.go
@@ -2,14 +2,47 @@ package main
 
 import (
   "log"
+  "encoding/json"
   fp "path/filepath"
   lv "github.com/jmhodges/levigo"
 )
 
+var wo *lv.WriteOptions
+var ro *lv.ReadOptions
+
+type dbMap struct {
+  name string
+  db *lv.DB
+}
+
+func openMap(name string, cache, block int) dbMap {
+  var dbm dbMap
+  opts := lv.NewOptions()
+  if cache != 0 {
+  opts.SetCache(lv.NewLRUCache(160*1024))
+  opts.SetBlockSize(16*1024)
+  opts.SetCreateIfMissing(true)
+
+  var e error
+  dbc.db, e = lv.Open(fp.Join("ldb", "user"), opts)
+  if e != nil {
+    log.Printf("DB ERROR in newDB: %v", e)
+    return nil
+  }
+  opts.Close()
+}
+
 type dbConn struct {
   db *lv.DB
-  wo *lv.WriteOptions
-  ro *lv.ReadOptions
+}
+
+type dbUser struct {
+  id []byte
+  firstName string
+  lastName string
+  userName string
+  email string
+  password string
 }
 
 func (dbc *dbConn) closeDB() {
@@ -27,6 +60,22 @@ func (dbc *dbConn) closeDB() {
   }
 }
 
+func (dbc *dbConn) get(k []byte) []byte {
+  v, e := dbc.db.Get(dbc.ro, k)
+  if e != nil {
+    log.Printf("DB ERROR in get: %v", e)
+    return nil
+  }
+  return v
+}
+
+func (dbc *dbConn) put(k, v []byte) {
+  e := dbc.db.Put(k, b)
+  if e != nil {
+    log.Printf("DB ERROR in put: %v", e)
+  }
+}
+
 func newDB() *dbConn {
   var dbc dbConn
 
@@ -38,7 +87,7 @@ func newDB() *dbConn {
   var e error
   dbc.db, e = lv.Open(fp.Join("ldb", "user"), opts)
   if e != nil {
-    log.Printf("%v", e)
+    log.Printf("DB ERROR in newDB: %v", e)
     return nil
   }
   opts.Close()
@@ -52,11 +101,17 @@ func newDB() *dbConn {
   val[2] = 0
   val[3] = 0
 
-  v, _ := dbc.db.Get(dbc.ro, []byte("igorsd"))
+  v, _ := dbc.db.Get(dbc.ro, []byte("igord"))
   log.Printf("%v", v)
 
   dbc.db.Put(dbc.wo, []byte("igord"), val)
   log.Printf("Ura!")
 
   return &dbc
+}
+
+func (dbc *dbConn) readUser(string ) *dbUser {
+  var user dbUser
+  v := dbc.get(
+  return &user
 }

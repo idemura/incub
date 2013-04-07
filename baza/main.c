7d8317f20307590ece3dd2a04db72ffc2b39ceaf
Igor Demura <igord@google.com>
2013-04-06 20:02:15 -0700

    Move TODOs from main.c to new TODO file

--- baza/main.c
+++ baza/main.c
@@ -16,28 +16,6 @@
 #include "defs.h"
 #include <stdio.h>
 
-/*
-TODO:
------
--- Use stack and refactor splitting.
--- Make compare function in BTree.
--- Binary search in BTree node.
-Make linked list on leaves (bottom level nodes).
-
-Mem counter: atomic add/sub
-
-Multithreaded btree. Smart lock of nodes. Have no idea how to do this.
-Btree disk operations. I/O queue. File chunks allocator.
-
-Command and interpreter. Do I need VM?
-
-HTTP server. This will just report something. Main thing is to listen for
-commands.
-
-Add allocator for btree nodes, because it allocs same block size always,
-possible optimizations.
-*/
-
 int main()
 {
     printf("main\n");

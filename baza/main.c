779befb29d20d6ec1599d3db786557c4fa5c6843
Igor Demura <igord@google.com>
2013-04-06 19:39:33 -0700

    Update TODO

--- baza/main.c
+++ baza/main.c
@@ -22,21 +22,20 @@ TODO:
 -- Use stack and refactor splitting.
 -- Make compare function in BTree.
 Binary search in BTree node.
-Mem counter: atomic add/sub
-
-Add allocator for btree nodes, because it allocs same block size always,
-possible optimizations.
-
 Make linked list on leaves (bottom level nodes).
 
-Multithreaded btree. Smart lock of nodes. Have no idea how to do this.
+Mem counter: atomic add/sub
 
+Multithreaded btree. Smart lock of nodes. Have no idea how to do this.
 Btree disk operations. I/O queue. File chunks allocator.
 
 Command and interpreter. Do I need VM?
 
 HTTP server. This will just report something. Main thing is to listen for
 commands.
+
+Add allocator for btree nodes, because it allocs same block size always,
+possible optimizations.
 */
 
 int main()

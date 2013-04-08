e644b949ef378b774885c724bbebd1987e1d68d8
Igor Demura <igord@google.com>
2013-04-08 15:53:27 -0700

    Time funcs test

--- baza/defs_test.c
+++ baza/defs_test.c
@@ -1,23 +1,24 @@
 #include "defs.c"
 #include "test.h"
 
+static void defs_time_test()
+{
+    struct timeval start, end, res;
+    end.tv_sec = 21;
+    end.tv_usec = 600000;
+    start.tv_sec = 10;
+    start.tv_usec = 500000;
+    timer_diff(&end, &start, &res);
+    TEST_CHECK(res.tv_sec == 11 && res.tv_usec == 100000);
+    start.tv_sec = 10;
+    start.tv_usec = 850000;
+    timer_diff(&end, &start, &res);
+    TEST_CHECK(res.tv_sec == 10 && res.tv_usec == 750000);
+}
+
 void defs_test()
 {
     test_begin("Defs");
-
-    struct timeval start, end, res;
-    end.tv_sec = 21;
-    end.tv_usec = 600;
-
-    start.tv_sec = 10;
-    start.tv_usec = 500;
-    timer_diff(&end, &start, &res);
-    TEST_CHECK(res.tv_sec == 11 && res.tv_usec == 100);
-
-    start.tv_sec = 10;
-    start.tv_usec = 700;
-    timer_diff(&end, &start, &res);
-    TEST_CHECK(res.tv_sec == 10 && res.tv_usec == 999800);
-
+    defs_time_test();
     test_end();
 }

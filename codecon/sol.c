7fc2e350bae73a46423c3a8a950141877b62ffa2
Igor Demura <igord@google.com>
2013-04-18 16:21:15 -0700

    Read input code

--- codecon/sol.c
+++ codecon/sol.c
@@ -13,6 +13,15 @@ int count(char *t1, char *t2, int n)
 int main(int argc, char **argv)
 {
     FILE *fin = get_input(argc, argv);
+    int n;
+    fscanf(fin, " %d", &n);
+    char *t1 = malloc(n + 1);
+    char *t2 = malloc(n + 1);
+    fscanf(fin, " %s", t1);
+    fscanf(fin, " %s", t2);
+    count(t1, t2, n);
+    free(t1);
+    free(t2);
     close_input(fin);
     return 0;
 }

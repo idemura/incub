37954253c2af42f1f4eaf7a01dd578b0d4f287a3
Igor Demura <igord@google.com>
2013-04-18 10:27:07 -0700

    Minor stuff

--- codecon/sol.c
+++ codecon/sol.c
@@ -2,13 +2,40 @@
 #include <memory.h>
 #include <stdio.h>
 
-int count(char *t1, char *t1, int n)
-{
+FILE *get_input(int argc, char **argv);
+void close_input(FILE *fin);
 
+int count(char *t1, char *t2, int n)
+{
+    return 0;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("main\n");
+    FILE *fin = get_input(argc, argv);
+    close_input(fin);
     return 0;
+}
+
+FILE *get_input(int argc, char **argv)
+{
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "-f") == 0) {
+            if (i + 1 < argc) {
+                FILE *f = fopen(argv[i + 1], "rt");
+                if (f) {
+                    return f;
+                }
+            }
+            break;
+        }
+    }
+    return stdin;
+}
+
+void close_input(FILE *fin)
+{
+    if (fin && fin != stdin) {
+        fclose(fin);
+    }
 }

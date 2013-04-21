e341f70c56c71574818cc48828b61bc58f97950b
Igor Demura <igord@google.com>
2013-04-21 14:50:17 -0700

    Solution for cf-296b

--- codecon/cf-296b.c
+++ codecon/cf-296b.c
@@ -10,32 +10,17 @@ void close_input(FILE *fin);
 
 int addmod(int x, int y)
 {
-    long long z = (long long)x + (long long)y;
-    if (z >= MOD) {
-        return (int)(z % MOD);
-    } else {
-        return (int)z;
-    }
+    return ((long long)x + (long long)y) % MOD;
 }
 
 int submod(int x, int y)
 {
-    long long z = (long long)x - (long long)y + MOD;
-    if (z >= MOD) {
-        return (int)(z % MOD);
-    } else {
-        return (int)z;
-    }
+    return ((long long)x - (long long)y + MOD) % MOD;
 }
 
 int mulmod(int x, int y)
 {
-    long long z = (long long)x * (long long)y;
-    if (z >= MOD) {
-        return (int)(z % MOD);
-    } else {
-        return (int)z;
-    }
+    return ((long long)x * (long long)y) % MOD;
 }
 
 int powmod(int x, int p)
@@ -52,9 +37,10 @@ int powmod(int x, int p)
 
 int is_comparable(int n, char *m1, char *m2)
 {
+    int i;
     int ls = 0;
     int gt = 0;
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         ls += m1[i] <= m2[i];
         gt += m1[i] >= m2[i];
     }
@@ -63,25 +49,29 @@ int is_comparable(int n, char *m1, char *m2)
 
 int check_step(int n, char *m1, char *m2, int i)
 {
+    int j, k;
+    int acc = 0;
+    char *p = 0;
+
     if (i == n) {
         int c = is_comparable(n, m1, m2);
-        // if (c) {
-        //     printf("%s\n%s\n\n", m1, m2);
-        // }
+        /*
+        if (c) {
+             printf("%s\n%s\n\n", m1, m2);
+        }
+        */
         return !c;
     }
 
-    int acc = 0;
-
     if (m1[i] == '?' && m2[i] == '?') {
-        for (int j = '0'; j <= '9'; ++j) {
+        for (j = '0'; j <= '9'; ++j) {
             m1[i] = j;
-            for (int k = '0'; k <= '9'; ++k) {
+            for (k = '0'; k <= '9'; ++k) {
                 m2[i] = k;
                 acc += check_step(n, m1, m2, i + 1);
             }
         }
-        // Important to return ? back
+        /* Important to return ? back */
         m1[i] = m2[i] = '?';
         return acc;
     }
@@ -90,12 +80,12 @@ int check_step(int n, char *m1, char *m2, int i)
         return check_step(n, m1, m2, i + 1);
     }
 
-    char *p = (m1[i] == '?'? m1: m2) + i;
-    for (int j = '0'; j <= '9'; ++j) {
+    p = (m1[i] == '?'? m1: m2) + i;
+    for (j = '0'; j <= '9'; ++j) {
         *p = j;
         acc += check_step(n, m1, m2, i + 1);
     }
-    // Important to return ? back
+    /* Important to return ? back */
     *p = '?';
 
     return acc;
@@ -108,14 +98,16 @@ int check(int n, char *m1, char *m2)
 
 int count(int n, char *m1, char *m2)
 {
-    // printf("m1 %s\nm2 %s\nn = %d\n", m1, m2, n);
-
+    int i;
     int gt = 1, ls = 1;
-    int no_q = 0; // Need this because mod arithmetics.
+    int no_q = 0; /* Need this because mod arithmetics. */
     int comb = 1;
     int q2 = 0;
     int eq = 1;
-    for (int i = 0; i < n; ++i) {
+    int twice = 0;
+    int comparable = 0;
+
+    for (i = 0; i < n; ++i) {
         int qa = m1[i] == '?';
         int qb = m2[i] == '?';
         if (qa && qb) {
@@ -124,7 +116,7 @@ int count(int n, char *m1, char *m2)
             gt = mulmod(gt, 55);
             comb = mulmod(comb, 100);
         } else if (qa || qb) {
-            // ls_i and gt_i are less or equal and greater or equal count.
+            /* ls_i and gt_i are less or equal and greater or equal count. */
             int ls_i = m1[i] == '?' ? m2[i] - '0' : '9' - m1[i];
             int gt_i = ('9' - '0') - ls_i;
             ls_i++;
@@ -133,7 +125,6 @@ int count(int n, char *m1, char *m2)
             gt = mulmod(gt, gt_i);
             comb = mulmod(comb, 10);
         } else {
-            no_q++;
             int diff = m1[i] - m2[i];
             if (diff != 0) {
                 eq = 0;
@@ -143,41 +134,48 @@ int count(int n, char *m1, char *m2)
                     ls = 0;
                 }
             }
+            no_q++;
         }
     }
 
     if (no_q == n) {
-        // This is pair without wild card. Answer depends on if this pair
-        // is comparable itself or not. If `ls` and `gt` are zeros this pair
-        // is incomparable.
+        /* This is pair without wild card. Answer depends on if this pair
+           is comparable itself or not. If `ls` and `gt` are zeros this pair
+           is incomparable.
+        */
         return ls == 0 && gt == 0 ? 1 : 0;
     }
 
-    // Equal pair counted twice in some combinations.
-    int twice = (q2 ? powmod(10, q2) : 1) * eq;
-    int comparable = submod(addmod(ls, gt), twice);
+    /* Equal pair counted twice in some combinations. */
+    twice = (q2 ? powmod(10, q2) : 1) * eq;
+    comparable = submod(addmod(ls, gt), twice);
     return submod(comb, comparable);
 }
 
 int main(int argc, char **argv)
 {
-    FILE *fin = get_input(argc, argv);
+    char *m1 = 0;
+    char *m2 = 0;
+    int n_count = 0;
     int n;
+    FILE *fin = get_input(argc, argv);
     fscanf(fin, " %d", &n);
     if (n <= 0) {
         return -1;
     }
-    char *m1 = malloc(n + 1);
+    m1 = malloc(n + 1);
     fscanf(fin, " %s", m1);
-    char *m2 = malloc(n + 1);
+    m2 = malloc(n + 1);
     fscanf(fin, " %s", m2);
-    int n_count = count(n, m1, m2);
+    n_count = count(n, m1, m2);
     printf("%d\n", n_count);
-    // int n_check = check(n, m1, m2);
-    // printf("check %d\n", n_check);
-    // if (n_check == n_count) {
-    //     printf("OK!\n");
-    // }
+    /*
+    int n_check = check(n, m1, m2);
+    printf("check %d\n", n_check);
+    if (n_check == n_count) {
+        printf("OK!\n");
+    }
+    */
     free(m1);
     free(m2);
     close_input(fin);
@@ -186,7 +184,8 @@ int main(int argc, char **argv)
 
 FILE *get_input(int argc, char **argv)
 {
-    for (int i = 0; i < argc; ++i) {
+    int i;
+    for (i = 0; i < argc; ++i) {
         if (strcmp(argv[i], "-f") == 0) {
             if (i + 1 < argc) {
                 FILE *f = fopen(argv[i + 1], "rt");

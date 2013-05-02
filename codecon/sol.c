c2c19d98976878db0216e6ebca63372312c6a2e7
Igor Demura <igord@google.com>
2013-05-02 09:46:54 -0700

    Problem

--- codecon/sol.c
+++ codecon/sol.c
@@ -8,7 +8,12 @@
 
 #define MOD 1000000007
 
-int nth_digit(int n, int j)
+int a, b, n;
+
+/* Gets nth digit of `n` in decimal form. Digits counted from the rightmost,
+   least significant, which is 0
+*/
+int nth_digit(int j, int n)
 {
     while (n != 0) {
         int d = n % 10;
@@ -21,9 +26,34 @@ int nth_digit(int n, int j)
     return 0;
 }
 
+int get_num(int k)
+{
+    return b * k + (n - k) * a;
+}
+
+int digit_lower_bound(int d, int pos, int lo, int hi)
+{
+    // int dlo = nth_digit(pos, get_num(lo));
+    // int dhi = nth_digit(pos, get_num(hi));
+    // if (nlo < d && nhi < d || nlo > d && nhi > d) {
+    //     return -1;
+    // }
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        int nmid = get_num(mid);
+        int dmid = nth_digit(pos, nmid);
+        if (d > dmid) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    return lo;
+}
+
 int main(int argc, char **argv)
 {
-    int a, b, n = 0, i, j;
+    int i, j;
 
     scanf("%d%d%d", &a, &b, &n);
     if (a > b) {
@@ -35,6 +65,14 @@ int main(int argc, char **argv)
     // Min and max sum of digits
     int min_sum = a * n;
     int max_sum = b * n;
+    a = 3;
+    b = 7;
+    n = 14;
+    int lb = digit_lower_bound(5, 1, 0, n + 1);
+    printf("lb %d\n", lb);
+    printf("this num is %d\n", get_num(lb));
+    printf("prev num is %d\n", get_num(lb - 1));
+    printf("next num is %d\n", get_num(lb + 1));
 
     printf("%d\n", nth_digit(123, 0));
     printf("%d\n", nth_digit(123, 1));

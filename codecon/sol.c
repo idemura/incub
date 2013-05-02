de10ba4f84e982486bf6080137b5bb06db937ec2
Igor Demura <igord@google.com>
2013-05-01 23:03:04 -0700

    New problem

--- codecon/sol.c
+++ codecon/sol.c
@@ -6,51 +6,40 @@
 #define ARRAY_SIZEOF(a) (sizeof(a) / sizeof(a[0]))
 #define ZERO(p, n)      memset(p, 0, n);
 
-int find_max(int *m, int n, int *d, int i0, int j0)
+#define MOD 1000000007
+
+int nth_digit(int n, int j)
 {
-    int i, j;
-    int max_sum = -1000000000;
-    ZERO(d, n * n * sizeof(*d));
-    for (i = i0; i < n; ++i) {
-        for (j = j0; j < n; ++j) {
-            int fi = i * n + j; /* flat index */
-            int sum1 = i > i0? d[fi - n]: 0;
-            int sum2 = j > j0? d[fi - 1]: 0;
-            int sum3 = i > i0 && j > j0? d[fi - n - 1]: 0;
-            int sum = sum1 + sum2 - sum3 + m[fi];
-            if (sum > max_sum) {
-                max_sum = sum;
-            }
-            d[fi] = sum;
+    while (n != 0) {
+        int d = n % 10;
+        n /= 10;
+        if (j == 0) {
+            return d;
         }
+        j--;
     }
-    return max_sum;
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    int n = 0, i, j;
-    int *d, *m;
-    int max_sum = -1000000000;
+    int a, b, n = 0, i, j;
 
-    scanf("%d", &n);
-    m = malloc(n * n * sizeof(*m));
-    for (i = 0; i < n * n; ++i) {
-        scanf("%d", &m[i]);
+    scanf("%d%d%d", &a, &b, &n);
+    if (a > b) {
+        int temp = a;
+        a = b;
+        b = temp;
     }
 
-    d = malloc(n * n * sizeof(*d));
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j) {
-            int sum = find_max(m, n, d, i, j);
-            if (sum > max_sum) {
-                max_sum = sum;
-            }
-        }
-    }
-    free(m);
-    free(d);
+    // Min and max sum of digits
+    int min_sum = a * n;
+    int max_sum = b * n;
 
-    printf("%d\n", max_sum);
+    printf("%d\n", nth_digit(123, 0));
+    printf("%d\n", nth_digit(123, 1));
+    printf("%d\n", nth_digit(123, 2));
+    printf("%d\n", nth_digit(123, 3));
+
     return 0;
 }

db3ee257ac801031ea4edfb04c4584972623f4c1
Igor Demura <igord@google.com>
2013-05-23 17:24:54 -0700

    Better sieve impl

--- codecon/lib.c
+++ codecon/lib.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 #define ARRAY_SIZEOF(a) (sizeof(a) / sizeof(a[0]))
 #define ZERO(p, n)      memset(p, 0, n);
@@ -150,8 +151,8 @@ void pascal_free(int **pas)
 }
 
 /* Takes `list` with size `list_n` as a buffer (no assumptions on values in it)
-   and runs Eratothenes sieve on it. Returns count of primes in list, which
-   stored in 0..list_n. Max prime can be found is list_n + 1.
+   and runs Eratosthenes sieve on it. Returns count of primes in list, which
+   stored in 0..list_n in the list. Max prime can be found is list_n + 1.
 
    Example of usage:
 
@@ -162,13 +163,27 @@ int sieve(int *list, int list_n)
 {
     ZERO(list, list_n * sizeof *list);
     int i, j, w = 0;
-    for (i = 0; i < list_n; ++i) {
+    int sqrt_n = (int)sqrt(list_n);
+    for (i = 0; i <= sqrt_n; ++i) {
         if (!list[i]) {
             int prime = i + 2;
-            for (j = i; j < list_n; j += prime) {
+            /* The first index to go, -2 for index shift (don't 0 and 1 take
+               into account).
+               Start from square of prime p. Let's assume the opposite: there
+               is some composite m = p * j, m < p * p, hence j < p.  Therefore,
+               this j should have some prime divisor p' <= j < p and hence,
+               m should be rejected in the process for j.
+            */
+            int first = prime * prime - 2;
+            for (j = first; j < list_n; j += prime) {
                 list[j] = 1;
             }
-            list[w++] = prime;
+        }
+    }
+    // Copy primes to the beginning.
+    for (i = 0; i < list_n; i++) {
+        if (!list[i]) {
+            list[w++] = i + 2;
         }
     }
     return w;

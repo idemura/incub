25bbaa5a7fcc685efbe2813c7b691f20de302b9d
Igor Demura <igord@google.com>
2013-07-07 16:32:17 -0700

    Clojure and new Timus problem

--- codecon/sol.cpp
+++ codecon/sol.cpp
@@ -12,37 +12,22 @@ using namespace std;
 
 typedef long long int lli;
 
-int n1, l1[50000], n2, l2[50000];
-const int SUM = 10000;
-
-void readlist(int *n, int *l)
-{
-    scanf("%d", n);
-    for (int i = 0; i < *n; i++) {
-        scanf("%d", l + i);
-    }
-}
+int N, K;
 
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("in", "r", stdin);
 #endif
-    int i1 = 0, i2 = 0;
-    // First list ascending, second - descending.
-    readlist(&n1, l1);
-    readlist(&n2, l2);
+    int i, j;
 
-    const char* ans = "NO";
-    for (; i1 < n1 && i2 < n2; ) {
-        int s = l1[i1] + l2[i2];
-        if (s == SUM) {
-            ans = "YES";
-            break;
-        }
-        s < SUM? i1++ : i2++;
+    scanf("%d%d", &N, &K);
+    for (i = 0; i < K; i++) {
+        char buf[16];
+        int l, r;
+        scanf("%d%d%s", &l, &r, buf);
+        bool odd = buf[0] == 'o';
     }
 
-    printf("%s\n", ans);
     return 0;
 }
--- exp/con_clj/src/con_clj/core.clj
+++ exp/con_clj/src/con_clj/core.clj
@@ -1,15 +1,35 @@
 (ns con-clj.core
   (:gen-class))
 
-(def squares (map #(* % %) (rest (range))))
+(def squares
+    (map #(* % %) (rest (range))))
 
 (defn sq-sum
   [n]
   (reduce + (take n squares)))
+
+(def fst first)
+(def snd (comp first rest))
+
+(defn fib [n]
+  (let [f (fn)]
+  (loop [i n a 0 b 1]
+        (if (= 0 i)
+            '()
+            (recur ))
+
+  (list 0 1 ())
+
+(defn des [s]
+  (let [[a b] s]
+       (+ a b)))
 
 (defn -main
   [& args]
   ; work around dangerous default behavior in Clojure
   (alter-var-root #'*read-eval* (constantly false))
   (println (sq-sum 4) "should be" (+ 1 4 9 16))
-  (println (take 5 squares)))
+  (println (take 5 squares))
+  (println (take 5 fib))
+  (println (des [1 2 3]))
+  )

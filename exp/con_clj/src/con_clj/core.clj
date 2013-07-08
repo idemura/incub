e1e7d7e2968911dbca4fcee0e3599ea8945e73ab
Igor Demura <igord@google.com>
2013-07-07 20:47:16 -0700

    Training Clojure

--- exp/con_clj/src/con_clj/core.clj
+++ exp/con_clj/src/con_clj/core.clj
@@ -1,28 +1,28 @@
-(ns con-clj.core
+(ns con-clj.demi
   (:gen-class))
 
 (def squares
     (map #(* % %) (rest (range))))
 
-(defn sq-sum
-  [n]
+(defn sq-sum [n]
   (reduce + (take n squares)))
 
 (def fst first)
 (def snd (comp first rest))
 
-(defn fib [n]
-  (let [f (fn)]
-  (loop [i n a 0 b 1]
-        (if (= 0 i)
-            '()
-            (recur ))
-
-  (list 0 1 ())
+(defn n-fib [n]
+  (defn gen [i a b]
+    (if (zero? i)
+        ()
+        (cons (+ a b) (gen (dec i) b (+ a b)))))
+  (gen n 0 1))
 
 (defn des [s]
   (let [[a b] s]
        (+ a b)))
+
+(defn primes [n]
+  (vec (range n)))
 
 (defn -main
   [& args]
@@ -30,6 +30,7 @@
   (alter-var-root #'*read-eval* (constantly false))
   (println (sq-sum 4) "should be" (+ 1 4 9 16))
   (println (take 5 squares))
-  (println (take 5 fib))
-  (println (des [1 2 3]))
+  (println (des '(10 2 3)))
+  (println (n-fib 7))
+  (println (map (partial * 100) (range 10)))
   )

0df76f3cc762da519431a957de0b761595437b3b
Igor Demura <igord@google.com>
2013-09-16 18:04:40 -0700

    Try to render a template

--- web/src/web/handler.clj
+++ web/src/web/handler.clj
@@ -5,8 +5,14 @@
     [compojure.route :as route]
     [net.cgrand.enlive-html :as html]))
 
+;; Can I insert it into ns decl?
+(html/set-ns-parser! html/xml-parser)
+
+(html/deftemplate index "templates/index"
+  [])
+
 (defroutes app-routes
-  (GET "/" [] "<p>Hello <b>World</b></p>")
+  (GET "/" [] (index))
   (GET "/ping/:what" [what] (str "<h1>Ping '" what "'</h1>"))
   (route/resources "/")
   (route/not-found "Not Found"))

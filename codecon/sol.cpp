cf4141f17d3c8329f8dd89395b2f6e65326354e8
Igor Demura <igord@google.com>
2013-12-05 15:08:26 -0800

    Interval tree

--- codecon/sol.cpp
+++ codecon/sol.cpp
@@ -9,102 +9,49 @@
 
 using namespace std;
 
-int findPowerOf2(int n)
-{
-    int p = 0;
-    for (; n % 2 == 0; n /= 2) {
-        p++;
-    }
-    return p;
+struct Interval {
+  float a, b;
+  Interval(float a, float b) : a(a), b(b) { }
+};
+
+struct Node {
+  float int_a, int_b;
+  Node *l, *r;
+  vector<Interval> ints;
+
+  Node(float int_a, float int_b)
+      : int_a(int_a), int_b(int_b), l(NULL), r(NULL) {
+  }
+};
+
+Node* createTree() {
+  Node* root = new Node(0, 8);
+  root->l = new Node(0, 4);
+  root->l->l = new Node(0, 2);
+  root->l->l->l = new Node(0, 1);
+  root->l->l->r = new Node(1, 2);
+  root->l->r = new Node(2, 4);
+  root->l->r->l = new Node(2, 3);
+  root->l->r->r = new Node(3, 4);
+  root->r = new Node(4, 8);
+  root->r->l = new Node(4, 6);
+  root->r->l->l = new Node(4, 5);
+  root->r->l->r = new Node(5, 6);
+  root->r->r = new Node(6, 8);
+  root->r->r->l = new Node(6, 7);
+  root->r->r->r = new Node(7, 8);
+  return root;
 }
 
-int power(int x, int n)
-{
-    if (n == 0) {
-        return 1;
-    }
-    int p = 1;
-    for (; n; n >>= 1) {
-        int overflow = INF / x;
-        if (n & 1) {
-            if (p > overflow) {
-                return INF;
-            }
-            p *= x;
-        }
-        if (x > overflow && n > 1) {
-            return INF;
-        }
-        x *= x;
-    }
-    return p;
-}
+void testIntervals(const std::vector<Interval>& ints) {
 
-int findLog(int x, int p)
-{
-    if (p == 0) {
-        return 1;
-    }
-    int a = 1, b = x;
-    while (a < b) {
-        int m = a + (b - a) / 2;
-        int m_p = power(m, p);
-        if (x > m_p) {
-            a = m + 1;
-        } else {
-            b = m;
-        }
-    }
-    return a;
-}
-
-bool solve()
-{
-    int i, j, a, b, t, n;
-
-    scanf("%d%d", &a, &b);
-    if (a == 0 && b == 0) {
-        return false;
-    }
-
-    if (b > a) {
-        t = a;
-        a = b;
-        b = t;
-    }
-    int p2 = findPowerOf2(a % 2? b : a);
-
-    // p2 is in [1,31] so just for is OK.
-    for (i = 0; i <= p2; i++) {
-        if (i != 0 && p2 % i != 0) {
-            continue;
-        }
-        n = findLog(b, i);
-        int npowi = power(n, i);
-        if (npowi == b && power(n + 1, i) == a) {
-            break;
-        }
-    }
-
-    int num = 0, height_sum = 0, l = 1, h = a;
-    for (j = 0; j < i; j++) {
-        height_sum += h * l;
-        num += l;
-        l *= n;
-        h /= (n + 1);
-    }
-    height_sum += h * l;
-
-    printf("%d %d\n", num, height_sum);
-    return true;
 }
 
 int main()
 {
 #ifndef ONLINE_JUDGE
-    freopen("in", "r", stdin);
+  freopen("in", "r", stdin);
 #endif
-    while (solve()) {
-    }
-    return 0;
+
+  return 0;
 }

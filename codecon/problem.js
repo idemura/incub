070a2956f390617f1f344b211603746caec98bda
Igor Demura <igord@google.com>
2014-01-29 10:44:14 -0800

    Comments

--- codecon/problem.js
+++ codecon/problem.js
@@ -8,13 +8,16 @@ function parens(n) {
     var i1, n1 = p1.length, i2, n2 = p2.length;
     for (i1 = 0; i1 < n1; i1++) {
       for (i2 = 0; i2 < n2; i2++) {
+        // To avoid duplications, only surround the first group.
         r.push('(' + p1[i1] + ')' + p2[i2]);
       }
     }
   }
 
+  // Size 0 and 1 generated, go 2..n.
   for (i = 2; i <= n; i++) {
     parMem[i] = [];
+    // Take i-1 parens and split them to the left and right group and combine.
     for (j = 0; j < i; j++) {
       combine(parMem[j], parMem[i - j - 1], parMem[i]);
     }

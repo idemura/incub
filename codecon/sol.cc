d421b42bc5662ad1e29e19d13428591731ac29c7
Igor Demura <igord@google.com>
2014-01-29 11:07:56 -0800

    LCS-like problem

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -13,36 +13,126 @@
 
 typedef long long int lli;
 
-int getCyclicLength(int n)
+int lcs_mem[84][84];
+int lcs(char *a, char *b)
 {
-  int cl = 1;
-  while (n != 1) {
-    if (n & 1) {
-      n = (3 * n + 1) >> 1;
-      cl += 2;
-    } else {
-      n >>= 1;
-      cl += 1;
+  memset(lcs_mem, 0, sizeof lcs_mem);
+  int an = strlen(a);
+  int bn = strlen(b);
+  for (int i = 1; i <= an; i++) {
+    for (int j = 1; j <= bn; j++) {
+      if (a[i - 1] == b[j - 1]) {
+        lcs_mem[i][j] = lcs_mem[i - 1][j - 1] + 1;
+      } else {
+        lcs_mem[i][j] = std::max(lcs_mem[i - 1][j], lcs_mem[i][j - 1]);
+      }
     }
   }
-  return cl;
+  return lcs_mem[an][bn];
 }
 
-// From problem statement I conclude labyrinth is a tree.
+char a[84], b[84], buf[84];
+int cs;
+
+void printLCS()
+{
+  int an = strlen(a);
+  int bn = strlen(b);
+  for (int i = 0; i <= an; i++) {
+    for (int j = 0; j <= bn; j++) {
+      printf("%3d", lcs_mem[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void routeRec(int i, int j, int iout)
+{
+  printf("In %d %d: %c %c\n", i, j, a[i-1], b[j-1]);
+  printf("%2d %2d\n", lcs_mem[i-1][j-1], lcs_mem[i-1][j]);
+  printf("%2d %2d\n", lcs_mem[i][j-1], lcs_mem[i][j]);
+  if (lcs_mem[i][j] < 0) {
+    // Already looked into it.
+    printf("return as visited\n");
+    printf("------ END\n");
+    return;
+  }
+  if (!lcs_mem[i][j]) {
+    buf[iout] = 0;
+    cs++;
+    printf("print result\n");
+    printf("%s\n", buf);
+    printf("------ END\n");
+    return;
+  }
+  int lcs_ij = lcs_mem[i][j];
+  lcs_mem[i][j] = -1;
+  if (a[i - 1] == b[j - 1]) {
+    printf("ai == bj\n");
+    buf[iout] = a[i - 1];
+    routeRec(i - 1, j - 1, iout + 1);
+  } else {
+    int branches = 0;
+    int t = 0;
+    // Neighbors 2D indices.
+    int neighbor[][2] = {{i - 1, j}, {i, j - 1}};
+    if (lcs_mem[i - 1][j] == lcs_mem[i][j - 1]) {
+      printf("neighbors are equal. chars: %c %c\n", a[i - 1], b[j - 1]);
+      assert(i >= 1 && j >= 1);
+      t = b[j - 1] < a[i - 1];
+    }
+    printf("t=%d, so: %d %d, %d %d\n", t, neighbor[t][0], neighbor[t][1],
+           neighbor[1-t][0], neighbor[1-t][1]);
+    int *ix = neighbor[t];
+    if (lcs_mem[ix[0]][ix[1]] == lcs_ij) {
+      printf("check branch 1\n");
+      routeRec(ix[0], ix[1], iout);
+      branches++;
+    }
+    ix = neighbor[1 - t];
+    if (lcs_mem[ix[0]][ix[1]] == lcs_ij) {
+      printf("check branch 2\n");
+      routeRec(ix[0], ix[1], iout);
+      branches++;
+    }
+    if (branches > 1) {
+      printf("branches %d\n", branches);
+    }
+  }
+  printf("------ END\n");
+}
+
+void revert(char *b, char *e)
+{
+  for (; b < e; b++, e--) {
+    std::swap(*b, *e);
+  }
+}
+
+void routes()
+{
+  int an = strlen(a);
+  int bn = strlen(b);
+  revert(a, a + an - 1);
+  revert(b, b + bn - 1);
+  lcs(a, b);
+  printLCS();
+  printf("lcs len %d\n", lcs_mem[an][bn]);
+  routeRec(an, bn, 0);
+}
+
 int main(int argc, char **argv)
 {
 // #ifndef ONLINE_JUDGE
 //   freopen("in", "r", stdin);
 // #endif
-  int a, b;
-  while (scanf("%d%d", &a, &b) == 2) {
-    int i0 = std::min(a, b);
-    int i1 = std::max(a, b);
-    int cl_max = 1;
-    for (int i = i0; i <= i1; i++) {
-      cl_max = std::max(cl_max, getCyclicLength(i));
-    }
-    printf("%d %d %d\n", a, b, cl_max);
+  int t = 0;
+  scanf("%d", &t);
+  while (t-- > 0) {
+    scanf("%s%s", a, b);
+    printf("%s\n%s\n", a, b);
+    routes();
+    printf("%d\n", cs);
   }
   return 0;
 }

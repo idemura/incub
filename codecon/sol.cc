63589f9be02d71f261a63a66ea9337711eb765a4
Igor Demura <igord@google.com>
2014-02-03 20:13:33 -0800

    Merged

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -14,57 +14,66 @@
 
 typedef long long int lli;
 
-int vs[120], last[120];
-int vs_n, c_n;
-
-// Returns true if sequence `vs[vs_i]` consists of all equal numbers.
-bool isConst(int n)
+int gcd(int a, int b)
 {
-  for (int i = 1; i < n; i++) {
-    if (vs[i - 1] != vs[i])
-      return false;
+  // Assumes `b < a`, otherwise the first iteration will swap `a` and `b`.
+  while (b != 0) {
+    int t = a % b;
+    a = b;
+    b = t;
   }
   return true;
 }
 
-// Returns step after that all differences equal. Sets up `last` array.
-int expandToConstDifferences()
+int cs[500000];  // It's too much tough:)
+lli Cnk(int n, int k)
 {
-  for (int i = 0, n = vs_n; ; i++, n--) {
-    last[i] = vs[n - 1];
-    if (isConst(n)) {
-      return i;
-    }
-    for (int j = 1; j < n; j++) {
-      vs[j - 1] = vs[j] - vs[j - 1];
+  if (n - k < k) {
+    k = n - k;
+  }
+  for (int i = 0; i < k; i++) {
+    cs[i] = n - i;
+  }
+  for (int j = k; j > 1; j--) {
+    int dj = n % j, d = j;
+    for (; d > 1; dj += j) {
+      int g = gcd(cs[dj], d);
+      if (g > 1) {
+        cs[dj] /= g;
+        d /= g;
+      }
     }
   }
-  return -1;  // Never here.
+  lli p = 1;
+  for (int i = 0; i < k; i++) {
+    p *= cs[i];
+  }
+  return p;
 }
 
 void solve()
 {
-  scanf("%d%d", &vs_n, &c_n);
-  for (int i = 0; i < vs_n; i++) {
-    scanf("%d", vs + i);
-  }
-  // Expand to differences. With polynomial, it will be all constant or one
-  // single value at some step `ci < vs_n`.
-  int ci = expandToConstDifferences();
-  // Make steps back from differences to values.
-  for (int j = 0; j < c_n; j++) {
-    vs[j] = last[ci];
-  }
-  for (; ci-- > 0; ) {
-    vs[0] += last[ci];
-    for (int j = 1; j < c_n; j++) {
-      vs[j] += vs[j - 1];
-    }
-  }
-  for (int j = 0; j < c_n; j++) {
-    printf("%d ", vs[j]);
-  }
-  printf("\n");
+  // So, we need count sums of k summands such that s1+s2+...sk=n and each
+  // si>=1. We can chop 1 from each summand and re-phrase: s1+s2+...sk=m,
+  // si>=0, m=n-k.
+  // For later we have recursive solution F(k, n):
+  // F(1, m) = 1
+  // F(k, 0) = 1
+  // F(k, m) = SUM i=0..m: F(k-1, m-i)
+  // Consider F(k, n) and F(k, m-1):
+  //   F(k,m)  =F(k-1,m)+F(k-1,m-1)+F(k-1,m-2)+...+F(k-1,0)
+  //   F(k,m-1)=         F(k-1,m-1)+F(k-1,m-2)+...+F(k-1,0)
+  // Tails are the same, so we have:
+  //   F(k, m) = F(k-1, m) + F(k, m-1)
+  // This is almost binomial coefficient! Consider the table, binomial
+  // coefficient are on diagonals. So we have:
+  //   F(k, m) = C(k-1, m+k-1).
+  // Return back to initial notation:
+  //   F(k, m) = C(k-1, n-1).
+  // So all we have to do is just compute binomial coefficient.
+  int n, k;
+  scanf("%d%d", &n, &k);
+  printf("%lld\n", Cnk(n - 1, k - 1));
 }
 
 int main(int argc, char **argv)

2f02d1ff0141fba870dfce6d5bae217c6833f727
Igor Demura <igord@google.com>
2014-02-20 12:57:59 -0800

    KMP string search algorithm

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -26,38 +26,7 @@ void printPrefixFn(const char *s, int *pf)
   printf("\n");
 }
 
-bool isPrefix(const char *s, int substr_len, int prefix_len)
-{
-  int suffix_first = substr_len - prefix_len;
-  for (int i = 0; i < prefix_len; i++) {
-    if (s[i] != s[suffix_first + i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-void naivePrefixFn(const char *s, int *pf)
-{
-  // Fill every index of prefix function. pf[i] is for the left substring of
-  // length `i+1`.
-  for (int i = 0; s[i]; i++) {
-    // Check prefixes of length 0,1,...i.
-    int l = 0;
-    for (int j = 0; j <= i; j++) {
-      if (isPrefix(s, i + 1, j)) {
-        l = j;
-      }
-    }
-    pf[i] = l;
-  }
-}
-
-// Computes prefix function for a string `s` and saves into `pf`, which is
-// allocated before the call. `pf[i]` is maximum length of an own (non-trivial)
-// prefix which is also a suffix of string `s[0..i]`, of length `i+1`:
-//    s[j] == s[sf+j], j=0 .. pf[i]-1,
-// where `sf=i+1-pf[i]` and stands for "suffix first".
+// Prefix function from alg/prefix-fn.cc.
 void computePrefixFn(const char *s, int *pf)
 {
   if (*s == 0) {
@@ -67,33 +36,52 @@ void computePrefixFn(const char *s, int *pf)
   for (int i = 1; s[i]; i++) {
     int k = pf[i - 1];
     for (; k != 0 && s[i] != s[k];) {
-      // Because for string of length `k` we store prefix function value at
-      // index k - 1.
       k = pf[k - 1];
     }
     pf[i] = k + (s[i] == s[k]);
   }
 }
 
-void testPrefixFn(const char *s)
+int kmpSearch(const char *s, const char *n, int *pf)
+{
+  int i = 0, j = 0;
+  while (s[i] && n[j]) {
+    if (s[i] == n[j]) {
+      i++;
+      j++;
+      continue;
+    }
+    if (j == 0) {
+      i++;
+      continue;
+    }
+    // Try another (shorter) prefix, possibly of length 0.
+    j = pf[j - 1];
+  }
+  return n[j]? -1: i - j;
+}
+
+int substrIndexNaive(const char *s, const char *n)
+{
+  const char *p = strstr(s, n);
+  return p? p - s: -1;
+}
+
+void testSubstrSearch(const char *s, const char *n)
 {
   const int s_len = strlen(s);
   int *pf = new int[s_len]();
-  int *pf_naive = new int[s_len]();
   computePrefixFn(s, pf);
   printf("Prefix function:\n");
   printPrefixFn(s, pf);
-  printf("CHECK:\n");
-  naivePrefixFn(s, pf_naive);
-  printPrefixFn(s, pf_naive);
-  for (int i = 0; i < s_len; i++) {
-    if (pf[i] != pf_naive[i]) {
-      printf(" PF distinct at %d\n", i);
-      break;
-    }
+  int i1 = kmpSearch(s, n, pf);
+  int i2 = substrIndexNaive(s, n);
+  printf("Search %s in %s\n", n, s);
+  printf("  KMP: %d check: %d\n", i1, i2);
+  if (i1 != i2) {
+    printf("FAILED!\n");
   }
   delete[] pf;
-  delete[] pf_naive;
 }
 
 int main(int argc, char **argv)
@@ -101,7 +89,9 @@ int main(int argc, char **argv)
 // #ifndef ONLINE_JUDGE
 //   freopen("in", "r", stdin);
 // #endif
-  testPrefixFn("ababdaab");
-  testPrefixFn("abcabdabcabcx");
+  testSubstrSearch("ababdaab", "abd");
+  testSubstrSearch("ababdaab", "aba");
+  testSubstrSearch("ababdaab", "aab");
+  testSubstrSearch("ababdaab", "aaa");
   return 0;
 }

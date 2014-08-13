87f88e7ad6d45f8af75117739c88b8ed114affdf
Igor Demura <igord@google.com>
2014-08-13 10:55:56 -0700

    Hares and Foxes continued

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -18,46 +18,46 @@
 
 typedef long long int lli;
 
-struct Pair {
-  double h, f;
+double a, b, c, d, h0, f0;
 
-  Pair(): h(), f() {}
-  Pair(double h, double f): h(h), f(f) {}
-};
-
-double a, b, c, d;
-Pair p0;
-
-Pair step(Pair p) {
-  return Pair(a * p.h - b * p.f,
-              c * p.f + d * p.h);
-}
-
-bool softZero(double x) {
-  return fabs(x) < 1E-9;
-}
-
-bool magnitude(Pair *p, double *m) {
-  double t = std::max(fabs(p->h), fabs(p->f));
-  if (softZero(t)) {
-    return false;
+void solve() {
+  double h = h0, f = f0;
+  // Warm-up.
+  for (int i = 0; i < 20; i++) {
+    double h1 = a * h - b * f;
+    double f1 = c * f + d * h;
+    h = h1;
+    f = f1;
   }
-  p->h /= t;
-  p->f /= t;
-  *m += log(t);
-  return true;
-}
 
-bool quadraticEq(double *x0, double *x1) {
-  double a_c = a - c;
-  double D = a_c * a_c - 4 * b * d;
-  if (D < 0) {
-    return false;
+  for (int t = 0; t < 3; t++) {
+    int hn = 0, hp = 0;
+    int fn = 0, fp = 0;
+    for (int i = 0; i < 10; i++) {
+      double h1 = a * h - b * f;
+      double f1 = c * f + d * h;
+      if (h1 < h) hn++; else hp++;
+      if (f1 < f) fn++; else fp++;
+      h = h1;
+      f = f1;
+    }
+    bool chaos = false;
+    if (fabs(h) < 0.5 && fabs(f) < 0.5) {
+      printf("Ecological balance will develop.\n");
+    } else if (hn == 0 && fn == 0) {
+      printf("Both hares and foxes will overgrow.\n");
+    } else if (hn == 0 && fp == 0) {
+      printf("Hares will overgrow while foxes will die out.\n");
+    } else if (hp == 0 && fp == 0) {
+      printf("Both hares and foxes will die out.\n");
+    } else if (hp == 0 && fn == 0) {
+      printf("Hares will die out while foxes will overgrow.\n");
+    } else {
+      chaos = true;
+    }
+    if (!chaos) return;
   }
-  double sqrt_D = sqrt(D);
-  *x0 = (a_c - sqrt_D) / (2 * d);
-  *x1 = (a_c + sqrt_D) / (2 * d);
-  return true;
+  printf("Chaos will develop.\n");
 }
 
 int main(int argc, char **argv) {
@@ -66,44 +66,9 @@ int main(int argc, char **argv) {
 #endif
   int n = 0;
   scanf("%d", &n);
-  const int kMaxStep = 120;
   for (int i = 0; i < n; i++) {
-    scanf("%lf%lf%lf%lf%lf%lf", &a, &b, &c, &d, &p0.h, &p0.f);
-    double x0, x1;
-    if (!quadraticEq(&x0, &x1)) {
-      printf("Chaos will develop.\n");
-      continue;
-    }
-
-    bool balance = false;
-    Pair p = p0;
-    double m = 0;
-    magnitude(&p, &m);
-    for (int i = 0; i < kMaxStep; i++) {
-      // printf("%.10lg %.10lg m = %.10lg\n", p.h, p.f, m);
-      p = step(p);
-      double m_prev = m;
-      if (!magnitude(&p, &m)) {
-        balance = true;
-        break;
-      }
-      if (fabs(m / m_prev - 1.0) < 1E-9) {
-        break;
-      }
-    }
-    if (balance) {
-      printf("Ecological balance will develop.\n");
-    } else {
-      if (p.h > 0 && p.f > 0) {
-        printf("Both hares and foxes will overgrow.\n");
-      } else if (p.h > 0 && p.f < 0) {
-        printf("Hares will overgrow while foxes will die out.\n");
-      } else if (p.h < 0 && p.f < 0) {
-        printf("Both hares and foxes will die out.\n");
-      } else {
-        printf("Hares will die out while foxes will overgrow.\n");
-      }
-    }
+    scanf("%lf%lf%lf%lf%lf%lf", &a, &b, &c, &d, &h0, &f0);
+    solve();
   }
   return 0;
 }

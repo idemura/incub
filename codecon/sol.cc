2388f823a56cb4432c7d75559b1b7e9dbb3088dc
Igor Demura <igord@google.com>
2014-08-13 11:12:33 -0700

    new

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -23,41 +23,38 @@ double a, b, c, d, h0, f0;
 void solve() {
   double h = h0, f = f0;
   // Warm-up.
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i < 8; i++) {
     double h1 = a * h - b * f;
     double f1 = c * f + d * h;
     h = h1;
     f = f1;
   }
 
-  for (int t = 0; t < 3; t++) {
-    int hn = 0, hp = 0;
-    int fn = 0, fp = 0;
-    for (int i = 0; i < 10; i++) {
-      double h1 = a * h - b * f;
-      double f1 = c * f + d * h;
-      if (h1 < h) hn++; else hp++;
-      if (f1 < f) fn++; else fp++;
-      h = h1;
-      f = f1;
-    }
-    bool chaos = false;
-    if (fabs(h) < 0.5 && fabs(f) < 0.5) {
-      printf("Ecological balance will develop.\n");
-    } else if (hn == 0 && fn == 0) {
-      printf("Both hares and foxes will overgrow.\n");
-    } else if (hn == 0 && fp == 0) {
-      printf("Hares will overgrow while foxes will die out.\n");
-    } else if (hp == 0 && fp == 0) {
-      printf("Both hares and foxes will die out.\n");
-    } else if (hp == 0 && fn == 0) {
-      printf("Hares will die out while foxes will overgrow.\n");
-    } else {
-      chaos = true;
-    }
-    if (!chaos) return;
+  int hn = 0, hp = 0;
+  int fn = 0, fp = 0;
+  for (int i = 0; i < 30; i++) {
+    double h1 = a * h - b * f;
+    double f1 = c * f + d * h;
+    if (h1 < h) hn++; else hp++;
+    if (f1 < f) fn++; else fp++;
+    h = h1;
+    f = f1;
+    printf("%.10lg %.10lg\n", h, f);
   }
-  printf("Chaos will develop.\n");
+  const int kMargin = 4;
+  if (fabs(h) < 0.5 && fabs(f) < 0.5) {
+    printf("Ecological balance will develop.\n");
+  } else if (hn < kMargin && fn < kMargin) {
+    printf("Both hares and foxes will overgrow.\n");
+  } else if (hn < kMargin && fp < kMargin) {
+    printf("Hares will overgrow while foxes will die out.\n");
+  } else if (hp < kMargin && fp < kMargin) {
+    printf("Both hares and foxes will die out.\n");
+  } else if (hp < kMargin && fn < kMargin) {
+    printf("Hares will die out while foxes will overgrow.\n");
+  } else {
+    printf("Chaos will develop.\n");
+  }
 }
 
 int main(int argc, char **argv) {

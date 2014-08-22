1ac89217fc8fabda824de120f27f1bae8cab85b9
Igor Demura <igord@google.com>
2014-08-22 11:03:38 -0700

    Buggy brackets solution

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -20,131 +20,110 @@ using namespace std;
 
 typedef long long int lli;
 
-struct ParenBal {
+struct Node {
   // Count of open right parens on the left end, and open left parens on the
   // right end.
   int r, l;
-  ParenBal(): r(), l() {}
+  Node(): r(), l() {}
 };
-typedef vector<ParenBal> ParenBalVector;
+typedef vector<Node> Bit;
 
-ParenBal fromChar(char c) {
-  ParenBal pb;
+Node fromChar(char c) {
+  Node pb;
   pb.r = c == ')'? 1: 0;
   pb.l = c == '('? 1: 0;
   return pb;
 }
 
-struct Bit {
-  int n;
-  vector<ParenBal> t;
-
-  Bit(): n() {}
-  void init(int str_len) {
-    int n = str_len;
-    int s = 0;
-    while (true) {
-      s += n;
-      if (n == 1) break;
-      n = (n + 1) / 2;
-    }
-    t.resize(s);
-    this->n = str_len;
+void init(Bit &bit, int leaf_n) {
+  int n = leaf_n;
+  int s = 0;
+  while (true) {
+    s += n;
+    if (n == 1) break;
+    n = (n + 1) / 2;
   }
+  bit.resize(s);
+}
 
-  void print() const {
-    int j = 0;
-    int n = this->n;
-    while (true) {
-      for (int i = 0; i < n; i++) {
-        printf("r %d l %d, ", t[j].r, t[j].l);
-        j++;
-      }
-      printf("\n");
-      if (n == 1) break;
-      n = (n + 1) / 2;
+void print(const Bit &bit, int leaf_n) {
+  int j = 0;
+  int n = leaf_n;
+  while (true) {
+    for (int i = 0; i < n; i++) {
+      printf("r %d l %d, ", bit[j].r, bit[j].l);
+      j++;
     }
+    printf("\n");
+    if (n == 1) break;
+    n = (n + 1) / 2;
   }
-};
+}
 
-ParenBal sum(const ParenBal &lh, const ParenBal &rh) {
-  ParenBal r;
+Node sum(const Node &lh, const Node &rh) {
+  Node r;
   int m = std::min(lh.l, rh.r);
   r.l = lh.l + rh.l - m;
   r.r = lh.r + rh.r - m;
   return r;
 }
 
-// int countStepsToSum(int n) {
-//   assert(n > 0);
-//   int c = 0;
-//   while (!(n & 1)) {
-//     c++;
-//     n >>= 1;
-//   }
-//   return c;
-// }
-
-void create(const vector<char> &str, Bit &bit) {
-  printf("string %s\n", &str[0]);
-  bit.init(str.size());
-  for (int i = 0; i < str.size(); i++) {
-    ParenBal pb = fromChar(str[i]);
-    bit.t[i] = pb;
-    int n = bit.n;
+void create(Bit &bit, const string &str) {
+  int leaf_n = str.size();
+  init(bit, leaf_n);
+  for (int i = 0; i < leaf_n; i++) {
+    Node pb = fromChar(str[i]);
+    bit[i] = pb;
+    int n = leaf_n;
     int s = n;
     int j = i;
-    printf("----\n");
     while (n > 1) {
-      printf("update index %d\n", s + j / 2);
-      bit.t[s + j / 2] = sum(bit.t[s + j / 2], pb);
+      bit[s + j / 2] = sum(bit[s + j / 2], pb);
       n = (n + 1) / 2;
       s += n;
       j /= 2;
     }
   }
-  bit.print();
 }
 
 bool check(const Bit &bit) {
-  ParenBal pb = bit.t[bit.t.size() - 1];
+  Node pb = bit[bit.size() - 1];
   return pb.r == 0 && pb.l == 0;
 }
 
-void update(int i, vector<char> &str, Bit &bit) {
+void update(Bit &bit, string &str, int i) {
+  int leaf_n = str.size();
   str[i] = str[i] == '('? ')': '(';
-  printf("new string %s\n", &str[0]);
-  bit.t[i] = fromChar(str[i]);
-  int n = bit.n;
+  bit[i] = fromChar(str[i]);
+  int n = leaf_n;
   int s = n;
   int j = i;
   while (n > 1) {
     if (j & 1) {
-      bit.t[s + j / 2] = sum(bit.t[j - 1], bit.t[j]);
+      bit[s + j / 2] = sum(bit[j - 1], bit[j]);
     } else {
-      bit.t[s + j / 2] = sum(bit.t[j], j + 1 < n? bit.t[j + 1]: ParenBal());
+      bit[s + j / 2] = sum(bit[j], j + 1 < n? bit[j + 1]: Node());
     }
     j /= 2;
     n = (n + 1) / 2;
     s += n;
   }
-  bit.print();
 }
 
 int main(int argc, char **argv) {
 #ifndef ONLINE_JUDGE
   freopen("in", "r", stdin);
 #endif
-  vector<char> s;
+  string str;
   Bit bit;
-  for (int i = 0; i < 1; i++) {
+  for (int i = 0; i < 10; i++) {
+    printf("Test %d:\n", i + 1);
     int n = 0;
     scanf("%d", &n);
-    s.resize(n);
-    scanf("%s", &s[0]);
+    str.resize(n);
+    scanf("%s", &str[0]);
 
-    create(s, bit);
-
+    create(bit, str);
     int m = 0;
     scanf("%d", &m);
     for (int j = 0; j < m; j++) {
@@ -154,7 +133,7 @@ int main(int argc, char **argv) {
         bool valid = check(bit);
         printf("%s\n", valid? "YES": "NO");
       } else {
-        update(op - 1, s, bit);
+        update(bit, str, op - 1);
       }
     }
   }

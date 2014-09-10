194a0a522b2140578341cffcb06ef55ad6cdb729
Igor Demura <igord@google.com>
2014-09-09 19:13:36 -0700

    UVa 147 in progress

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -23,50 +23,38 @@ using namespace std;
 
 typedef long long int lli;
 
-const int kCoins[] = { 
+const int kMax = 300;
+const int kCoins[] = {
     5/5, 10/5, 20/5, 50/5, // Cents
     100/5, 200/5, 500/5, 1000/5, 2000/5, 5000/5,
     10000/5 };  // 100$
 
-void printCoins() {
-  for (int i = 0; i < ARRAY_SIZEOF(kCoins); i++) {
-    printf("%d ", kCoins[i]);
-  }
-  printf("\n");
-}
+int mem[ARRAY_SIZEOF(kCoins)][kMax * 100 / 5 + 1];
 
-int stupidCountRec(int n, int ci, int depth) {
-  char tab[24] = {};
-  for (int i = 0; i < depth; i++)
-    tab[i] = ' ';
-
-  printf("%sn %d ci %d value %d\n", tab, n, ci, kCoins[ci]);
+int countRec(int n, int ci) {
   if (ci == 0 || n == 0) {
-    printf("%sci is 0, return 1\n", tab);
     return 1;
   }
-  int max_coins = n / kCoins[ci];
-  printf("%smax_coins %d\n", tab, max_coins);
-  int s = 0;
-  for (int i = 0; i <= max_coins; i++) {
-    s += stupidCountRec(n - i * kCoins[ci], ci - 1, depth + 1);
-    printf("%snew s %d\n", tab, s);
+  if (mem[ci][n] == 0) {
+    int max_coins = n / kCoins[ci];
+    int s = 0;
+    for (int i = 0; i <= max_coins; i++) {
+      s += countRec(n - i * kCoins[ci], ci - 1);
+    }
+    mem[ci][n] = s;
   }
-  printf("%sreturn %d\n", tab, s);
-  return s;
+  return mem[ci][n];
 }
 
-int stupidCount(int n) {
-  return stupidCountRec(n, ARRAY_SIZEOF(kCoins) - 1, 0);
+int count(int n) {
+  return countRec(n, ARRAY_SIZEOF(kCoins) - 1);
 }
 
 int main(int argc, char **argv) {
 #ifndef ONLINE_JUDGE
   freopen("in", "r", stdin);
 #endif
-
-  printCoins();
-  printf("%d\n", stupidCount(200/5));
+  printf("%d\n", count(200/5));
   return 0;
 }
 

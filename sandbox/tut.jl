f5ad565c0b68841d82aba1be3db46fdf84d93843
Igor Demura <igord@google.com>
2014-12-07 20:14:09 -0800

    Julia: play with sockets

--- sandbox/tut.jl
+++ sandbox/tut.jl
@@ -1,1 +1,16 @@
-# Sandbox
+# @async begin
+  counter = 1
+  listen_s = listen(2000)
+  while true
+    println("waiting on socket")
+    let s = accept(listen_s)
+      println("conn accepted")
+      # request = readall(s)
+      # println(request);
+      println("writing response $counter")
+      write(s, "Golish Polish $counter")
+      counter += 1
+      close(s)
+    end
+  end
+# end

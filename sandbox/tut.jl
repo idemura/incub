07d5b28b1d8785ee5c01851f6dd40fdfb8ef1352
Igor Demura <igord@google.com>
2015-01-02 10:06:17 -0800

    Julia experiments

--- sandbox/tut.jl
+++ sandbox/tut.jl
@@ -1,16 +1,34 @@
-# @async begin
-  counter = 1
-  listen_s = listen(2000)
-  while true
-    println("waiting on socket")
-    let s = accept(listen_s)
-      println("conn accepted")
-      # request = readall(s)
-      # println(request);
-      println("writing response $counter")
-      write(s, "Golish Polish $counter")
-      counter += 1
-      close(s)
+function bd_prob(k::Int)
+  p::Float64 = 1.0
+  for i in 0 .. k - 1
+    printlln(365.0 - i)
+    p *= (365.0 - i) / 365
+  end
+  1.0 - p
+end
+
+function same_sign(x1::Float64, x2::Float64)::Bool = sign(x1) == sign(x2)
+
+function bisection_eq(f::Function, x1::Float64, x2::Float64, eps::Float64)::Float64
+  f1 = f(x1)
+  f2 = f(x2)
+  if (f1 > 0 && f2 > 0) || (f1 < 0 && f2 < 0)
+    exit(-1)
+  end
+  while abs(x2 - x1) < eps
+    xm = (x1 + x2) * 0.5
+    fm = f(xm)
+    if same_sign(f1, fm)
+      x1 = xm
+      f1 = fm
+    else
+      x2 = xm
+      f2 = fm
     end
   end
-# end
+  x0
+end
+
+function p_nk(n::Int, k::Int)::BigInt = div(factorial(BigInt(n)) / factorial(BigInt(k)))
+
+println(p_nk(3, 2))

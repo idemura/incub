fd597bcf33ec82ff540c8ba096a6db60563c8170
Igor Demura <igord@google.com>
2015-01-16 19:20:02 -0800

    Timus probem

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -22,98 +22,47 @@
 
 using namespace std;
 
-typedef long long int lli;
-typedef vector<int> vector_int;
+typedef long long int i64;
+typedef __int128 i128;
 
-void printMapping(const vector_int &b_to_a) {
-  cout << "Mapping:" << endl;
-  for (int i = 0; i < b_to_a.size(); i++) {
-    cout << i << " - " << b_to_a[i] << endl;
-  }
+struct M2x2i {
+  i64 m11 = 0, m12 = 0, m21 = 0, m22 = 0;
+};
+
+i128 mul_i128(i64 a, i64 b) {
+  return i128(a) * i128(b);
 }
 
-int getProjectionSize(const vector<vector_int> &a) {
-  int n = -1;
-  for (int i = 0; i < a.size(); i++) {
-    for (int j = 0; j < a[i].size(); j++) {
-      if (a[i][j] > n) n = a[i][j];
+M2x2i mulMatrix(const M2x2i &a, const M2x2i &b, i64 m) {
+  M2x2i r;
+  r.m11 = (mul_i128(a.m11, b.m11) + mul_i128(a.m12, b.m21)) % m;
+  r.m12 = (mul_i128(a.m11, b.m12) + mul_i128(a.m12, b.m22)) % m;
+  r.m21 = (mul_i128(a.m21, b.m11) + mul_i128(a.m22, b.m21)) % m;
+  r.m22 = (mul_i128(a.m21, b.m12) + mul_i128(a.m22, b.m22)) % m;
+  return r;
+}
+
+M2x2i powMatrix(const M2x2i &a, i64 p, i64 m) {
+  M2x2i f = a, r;
+  r.m11 = r.m22 = 1;
+  for (i64 k = m; k != 0; k >>= 1) {
+    if (k & 1) {
+      r = mulMatrix(f, r, m);
     }
+    f = mulMatrix(f, f, m);
   }
-  return n + 1;
-}
-
-bool dfs_bipart(
-    const vector<vector_int> &a,
-    int v,  // Vertex in a.
-    vector_int &m,  // Marks on vertices of a.
-    vector_int &b_to_a) {
-  if (m[v]) return false;
-  m[v] = 1;
-
-  for (int i = 0; i < a[v].size(); i++) {
-    int bi = a[v][i];
-    if (b_to_a[bi] < 0 || dfs_bipart(a, b_to_a[bi], m, b_to_a)) {
-      b_to_a[bi] = v;
-      return true;
-    }
-  }
-  return false;
-}
-
-void kuhn(const vector<vector_int> &a, int b_size, vector_int &b_to_a) {
-  b_to_a.resize(b_size, -1);
-  for (int i = 0; i < a.size(); i++) {
-    vector_int m(a.size());
-    dfs_bipart(a, i, m, b_to_a);
-  }
-}
-
-void test(const vector<vector_int> &a) {
-  vector_int b_to_a;
-  kuhn(a, getProjectionSize(a), b_to_a);
-  printMapping(b_to_a);
+  return r;
 }
 
 int main(int argc, char **argv) {
-  {
-    vector<vector_int> a(4);
-    a[0].push_back(0);
-    a[0].push_back(1);
-    a[1].push_back(0);
-    a[1].push_back(1);
-    a[1].push_back(2);
-    a[2].push_back(1);
-    a[3].push_back(1);
-    a[3].push_back(2);
-    test(a);
-  }
-  {
-    vector<vector_int> a(4);
-    a[0].push_back(0);
-    a[0].push_back(1);
-    a[1].push_back(1);
-    a[1].push_back(0);
-    a[1].push_back(2);
-    a[2].push_back(1);
-    a[3].push_back(1);
-    a[3].push_back(2);
-    a[3].push_back(3);
-    test(a);
-  }
-  {
-    vector<vector_int> a(4);
-    a[0].push_back(0);
-    a[0].push_back(1);
-    a[1].push_back(1);
-    a[1].push_back(0);
-    a[1].push_back(2);
-    a[2].push_back(1);
-    a[3].push_back(1);
-    a[3].push_back(2);
-    a[3].push_back(3);
-    a[3].push_back(4);
-    test(a);
-  }
+  i64 n = 0, k = 0, m = 0;
+  cin >> n >> k >> m;
+  M2x2i r;
+  r.m11 = 0;
+  r.m12 = k - 1;
+  r.m21 = k - 1;
+  r.m22 = k - 1;
+  M2x2i r_to_n = powMatrix(r, n - 1, m);
+  cout << (r_to_n.m12 * (k - 1)) << endl;
   return 0;
 }
-

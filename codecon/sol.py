406b62cbc79b7471983f07c23c875e8532134351
Igor Demura <igord@google.com>
2015-02-23 19:23:48 -0800

    Timus 1017 in progress

--- codecon/sol.py
+++ codecon/sol.py
@@ -1,16 +1,29 @@
-# Timus problem 1024
 import math
 import sys
 
+def min_blocks_in_columns(k):
+  return k * (k + 1) // 2
+
+def sum_kth_down(a, k, n):
+  s = 0
+  i = n - k
+  while i >= 0:
+    s += a[i]
+    # if a[i] == 0: break?
+    i -= k
+  return s
+
+def dynamic(n):
+  d = [[0] * (n + 1), [0] + [1] * n]
+  cols = 2
+  while min_blocks_in_columns(cols) <= n:
+    d[cols] = [sum_kth_down(d[cols - 1], cols, i) for i in range(0, n + 1)]
+    cols += 1
+  s = 0
+  for i in range(1, cols):
+    s += d[i][n]
+  return s
+
 data_in = sys.stdin.read().split()
-positions = [int(x) for x in data_in[1:]]
-for p in positions:
-  g = int(math.sqrt(2 * p))
-  first1 = g * (g - 1) // 2 + 1;
-  first2 = g * (g + 1) // 2 + 1;
-  if p >= first2:
-    first = first2
-  else:
-    first = first1
-  print("1" if p == first else "0", end=' ')
-print()
+ints_in = [int(x) for x in data_in]
+dynamic(ints_in[0])

166dbac0256363565a3bac9028924303507247f0
Igor Demura <igord@google.com>
2015-03-12 13:17:47 -0700

    Timus 1016

--- codecon/sol.py
+++ codecon/sol.py
@@ -1,50 +1,52 @@
-import math
 import sys
 
-def split_dices(l):
-  n = 6
-  r = []
-  for k in range(0, len(l), n):
-    r.append(tuple(l[k : k + n]))
-  return r
+INF = 0x7fffffff
 
-def order(l):
-  k = 1
-  if l[2] < l[k]: k = 2
-  if l[3] < l[k]: k = 3
-  if l[4] < l[k]: k = 4
-  if k == 1: return (l[0], l[1], l[2], l[3])
-  if k == 2: return (l[0], l[2], l[3], l[4])
-  if k == 3: return (l[0], l[3], l[4], l[1])
-  if k == 4: return (l[0], l[4], l[1], l[2])
+def parse_pos(s):
+  return ("abcdefgh".find(s[0]), int(s[1]) - 1)
+def make_table(x):
+  return [[x] * 8 for _ in range(8)]
+def on_board(x, y):
+  return 0 <= x and x < 8 and 0 <= y and y < 8;
 
-def canonical(dice):
-  if dice[0] == 1:
-    return order((dice[1], dice[2], dice[3], dice[4], dice[5]))
-  if dice[1] == 1:
-    return order((dice[0], dice[2], dice[5], dice[4], dice[3]))
-  if dice[2] == 1:
-    return order((dice[4], dice[1], dice[3], dice[0], dice[5]))
-  if dice[3] == 1:
-    return order((dice[5], dice[2], dice[1], dice[4], dice[0]))
-  if dice[4] == 1:
-    return order((dice[2], dice[1], dice[5], dice[0], dice[3]))
-  if dice[5] == 1:
-    return order((dice[3], dice[2], dice[0], dice[4], dice[1]))
+def pq_min(pq):
+  v_min = INF
+  k_min = None
+  for k, v in pq:
+    if v < v_min:
+      k_min = k
+      v_min = v
+  return k_min, v_min
+
+# Neighbours in the order: up, right, bottom, left for each face of the cube in
+# the order they appear in the input.
+neibs_delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+neibs = [
+    (4, 3, 2, 5),
+    (2, 3, 4, 5),
+    (0, 3, 1, 5),
+    (1, 2, 0, 4),
+    (1, 3, 0, 5),
+    (1, 4, 0, 2),
+]
 
 data_in = sys.stdin.read().split()
-dices = split_dices([int(x) for x in data_in[1:]])
-dice_type = {}
-output_order = []
-for i, d in enumerate(dices):
-  c = canonical(d)
-  if c not in dice_type:
-    dice_type[c] = []
-    output_order.append(c)
-  dice_type[c].append(i + 1)
+start, end = parse_pos(data_in[0]), parse_pos(data_in[1])
+faces = [int(x) for x in data_in[2:]]
+min_known = {}
+# cost = make_table(INF)
+prev = make_table(None)
 
-print len(dice_type)
-for i in output_order:
-  for k in dice_type[i]:
-    print k,
-  print
+pq = {start : 0}
+p = None
+while len(pq) > 0:
+  (x, y), v = pq_min(pq)
+  min_known[(x, y)] = True
+  prev[(x, y)] = p
+  for dx, dy in neibs_delta:
+    xs = x + dx
+    ys = y + dy
+    if on_board(xs, ys):
+      vs = v +
+      if (xs, ys) in pq:
+        xs

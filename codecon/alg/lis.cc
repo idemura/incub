44f06ecb6595367389c866a903d193436e4f9243
Igor Demura <igord@google.com>
2015-04-28 09:47:04 -0700

    srm623d1-2

--- codecon/alg/lis.cc
+++ codecon/alg/lis.cc
@@ -1,9 +1,10 @@
 #include "base.h"
 
-int lis(const vector<int> &v) {
-  vector<int> incr{v[0]};
-  int len = 1;
-  for (int i = 1; i < v.size(); i++) {
+template<class T>
+int lis(const vector<T> &v) {
+  vector<T> incr;
+  int len = 0;
+  for (int i = 0; i < v.size(); i++) {
     auto u = upper_bound(incr.begin(), incr.end(), v[i]);
     incr.erase(u, incr.end());  // Same as resize.
     incr.push_back(v[i]);

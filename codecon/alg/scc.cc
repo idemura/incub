02592563b6162cbe9992de70e8de8d5bbfb5497f
Igor Demura <igord@google.com>
2015-05-24 12:23:08 -0700

    scc reduce graph

--- codecon/alg/scc.cc
+++ codecon/alg/scc.cc
@@ -1,41 +1,4 @@
-#include <algorithm>
-#include <functional>
-#include <iostream>
-#include <list>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <string>
-#include <queue>
-#include <vector>
-#include <memory>
-#include <sstream>
-#include <utility>
-#include <math.h>
-#include <stdlib.h>
-
-#define ARRAY_SIZEOF(A) (sizeof(A) / sizeof(A[0]))
-#define NON_COPYABLE(C) \
-    C(const C&) = delete; \
-    C& operator=(const C&) = delete;
-#define NEW_UNIQUE(T) unique_ptr<T>(new T)
-#define CHECK(E) \
-  do { \
-      if (!(E)) { \
-        cout << "CHECK failed at " << __FILE__ << "@" << __LINE__ << endl; \
-        exit(EXIT_FAILURE); \
-    } \
-  } while (false)
-
-using namespace std;
-
-using i64 = long long int;
-using i32 = int;
-
-constexpr char kEol[] = "\n";
-constexpr int INF = 0x7fffffff;
-constexpr int MOD = 100000007;
+#include "base.h"
 
 using VertexVec = vector<int>;
 using Edge = pair<int, int>;

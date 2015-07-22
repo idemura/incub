dafdb55bb35513e16d200307f1bac95246d312e9
Igor Demura <igord@google.com>
2015-07-22 13:06:18 -0700

    NEW! algorithms in MD!

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -19,45 +19,68 @@ using i32 = int;
 
 constexpr int INF = 0x7fffffff;
 constexpr int MOD = 100000007;
+constexpr int DIM = 13;
 
-int sqrt_int(int n) {
-  return static_cast<int>(sqrt(n));
-}
+using PairOfInts = pair<int, int>;
 
-vector<int> primes(int n) {
-  vector<int> sieve(n + 1), result;
-  auto i = 2, imax = sqrt_int(n);
-  for (; i <= imax; i++) {
-    if (sieve[i] != 0) continue;
-    result.push_back(i);
-    for (int j = i * i; j <= n; j += i) {
-      sieve[j] = 1;
+struct PairWithLen {
+  PairOfInts p;
+  int d = 0;
+  PairWithLen() {}
+  PairWithLen(int x, int y, int d): x(x), y(y), d(d) {}
+};
+
+void compute_dist(int v,
+    const vector<PairOfInts> &pos,
+    const vector<string> &m,
+    int *row) {
+  vector<PairWithLen> q{{pos[i].first, pos[i].second, 0}};
+  int f[DIM][DIM] = {};
+  for (int i = 0; i < q.size(); i++) {
+    int x = q[i].p.first, y = q[i].p.second;
+    if (f[x][y]) continue;
+    f[x][y] = 1;
+    if (m[x][y] == '*') {
+      row[pos.find(q[i].p) - pos.begin()] = q[i].len;
+    }
+    if (x - 1 >= 0 && !f[x - 1][y]) {
+      q.emplace_back(x - 1, y, q[i].d + 1);
+    }
+    if (y - 1 >= 0 && !f[x][y - 1]) {
+      q.emplace_back(x, y - 1, q[i].d + 1);
+    }
+    if (x + 1 < m.size() && !f[x + 1][y]) {
+      q.emplace_back(x + 1, y, q[i].d + 1);
+    }
+    if (y + 1 < m.size() && !f[x][y + 1]) {
+      q.emplace_back(x, y + 1, q[i].d + 1);
     }
   }
-  for (; i <= n; i++) {
-    if (sieve[i] == 0) result.push_back(i);
+}
+
+int read_solve(const vector<string> &m) {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> tr_pos{make_pair(0, 0)}, q;
+  vector<string> m(n);
+  for (int i = 0; i < n; i++) {
+    cin >> m[i];
+    for (int j = 0; j < n; j++) {
+      if (m[i][j] == '*') tr_pos.emplace_back(i, j);
+    }
   }
-  return move(result);
+  int a[DIM][DIM] = {};
+  for (int i = 0; i < tr_pos.size(); i++) {
+    compute_dist(i, tr_pos, m, a[i]);
+  }
 }
 
 int main(int argc, char **argv) {
   ios_base::sync_with_stdio(false);
-  constexpr int kNum = 10000;
-  vector<int> c(kNum + 1);
-  auto ps = primes(kNum);
-  for (auto a : ps) {
-    for (auto b : ps) {
-      int n = a + 2 * b;
-      if (n >= c.size()) break;
-      c[n]++;
-    }
-  }
   int t;
   cin >> t;
   for (; t > 0; t--) {
-    int n;
-    cin >> n;
-    cout << c[n] << endl;
+    cout << read_solve() << endl;
   }
   return 0;
 }

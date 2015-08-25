d26bff982118bf14e9533b64b062ecfca59cc765
Igor Demura <igor.demura@gmail.com>
2015-08-24 21:40:25 -0700

    fc in progress

--- codecon/sol.cc
+++ codecon/sol.cc
@@ -17,6 +17,7 @@ void fc_build_first(const vector<int> &l, vector<FC_Node> &nodes) {
   }
 }
 
+// Builds fixed fraction cascading (fraction parameter p = 0.5).
 void fc_build(const vector<vector<int>> &lists) {
   FC fc;
   fc.resize(lists.size());
@@ -26,17 +27,45 @@ void fc_build(const vector<vector<int>> &lists) {
     int k1 = 0;
     int kr = 0;
     // Previous cascaded node.
-    FC_Node *prev_c = nullptr;
-    fc.nodes[i].resize(lists[i].size() + fc.nodes[i - 1].size());
+    int prev_c = -1;
+    fc.nodes[i].resize(lists[i].size() + fc.nodes[i - 1].size()) / 2;
     while (k0 < fc.nodes[i - 1].size() && k1 < lists[i].size()) {
       if (lists[i][k1] < fc.nodes[i - 1][k0].n) {
-        fc.nodes[i][kr].n = list[i][k];
+        fc.nodes[i][kr].n = list[i][k1];
         fc.nodes[i][kr].f = 0;
         fc.nodes[i][kr].ptr = prev_c;
+        while (prev_c >= 0 && prev_c->ptr == nullptr) {
+          prev_c->ptr = &fc.nodes[i][kr];
+          prev_c--;
+
+        }
         kr++;
+        k1 += 1;
       } else {
+        fc.nodes[i][kr].n = lists[i][k0];
+        fc.nodes[i][kr].f = 1;
+        fc.nodes[i][kr].ptr = nullptr;  // Will be set.
+        prev_c = &fc.nodes[i][kr];
+        kr++;
+        k0 += 2;
       }
     }
+    while (k0 < fc.nodes[i - 1].size()) {
+      fc.nodes[i][kr].n = fc.nodes[i - 1][k0].n;
+      fc.nodes[i][kr].f = 1;
+      fc.nodes[i][kr].ptr = nullptr;
+      if (prev_c && prev_c->ptr == nullptr) {
+        prev_c->ptr = &fc.nodes[i][kr];
+      }
+      kr++;
+      k0 += 2;
+    }
+    while (k1 < lists[i].size()) {
+      fc.nodes[i][kr].n = lists[i][k1];
+      fc.nodes[i][kr].f = 0;
+      fc.nodes[i][kr].ptr = prev_c;
+      kr++;
+      k1 += 1;
 
   }
 }
@@ -45,6 +74,21 @@ void fc_search() {
 }
 
 int main(int argc, char **argv) {
+        if (prev_c && prev_c->ptr == nullptr) {
+        if (prev_c && prev_c->ptr == nullptr) {
+        if (prev_c && prev_c->ptr == nullptr) {
+        if (prev_c && prev_c->ptr == nullptr) {
+        if (prev_c && prev_c->ptr == nullptr) {
+          prev_c->ptr = &fc.nodes[i][kr];
+        }
+          prev_c->ptr = &fc.nodes[i][kr];
+        }
+          prev_c->ptr = &fc.nodes[i][kr];
+        }
+          prev_c->ptr = &fc.nodes[i][kr];
+        }
+          prev_c->ptr = &fc.nodes[i][kr];
+        }
   ios_base::sync_with_stdio(false);
   test();
   return 0;

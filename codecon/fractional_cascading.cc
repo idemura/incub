21ab2c111b6ebbd21465af142c0d916070ae51a4
Igor Demura <igor.demura@gmail.com>
2015-09-12 23:18:50 -0700

    update configs

--- codecon/fractional_cascading.cc
+++ codecon/fractional_cascading.cc
@@ -1,43 +1,49 @@
 #include "base.h"
 
-constexpr int kBlack = 0;
-constexpr int kWhite = 1;
-
 template<class T>
-using vecvec = vector<vector<T>>;
+class FCascade {
+public:
+  using ValueT = T;
 
-// I call nodes from the i-th list 'white' and nodes cascaded from (i-1)-th
-// level 'black'.
-struct Node {
-  int v = 0;  // Value.
-  int c = kBlack;  // Color.
-  // Inverted color index. Closest white to the right of a black node and
-  // closest to the left black node of a white node.
-  int j = 0;
-  int cascade_ix = 0;  // Cascaded index, for black node only.
+  FCascade() {}
+  DEFAULT_COPY(FCascade);
+
+  void clear() { cascades_.clear(); }
+  void add_list(const vector<ValueT> &a);
+  vector<pair<int, int>> search(ValueT x) const;
+  void print() const;
+
+private:
+  static constexpr int kBlack = 0;
+  static constexpr int kWhite = 1;
+
+  // I call nodes from the i-th list 'white' and nodes cascaded from (i-1)-th
+  // level 'black'.
+  struct Node {
+    ValueT v = 0;  // Value.
+    int c = kBlack;  // Color.
+    // If c == kBlack: next white to the right, OR
+    // If c == kWhite: next black to the left.
+    int j = 0;
+    int cascade_ix = 0;  // Cascaded index, for black node only.
+  };
+
+  static void build_cascade(const vector<ValueT> &w, const vector<Node> &b,
+                            vector<Node> &res);
+
+  list<vector<Node>> cascades_;
 };
-
-struct FCascade {
-  vecvec<Node> nodes;
-};
-
-vector<Node> build_first(const vector<int> &a) {
-  vector<Node> res(a.size());
-  for (int i = 0; i < a.size(); i++) {
-    res[i].v = a[i];
-    res[i].c = kWhite;
-    res[i].j = res[i].cascade_ix = -1;
-  }
-  return res;
-}
 
 // For white, we do 2 things:
 //   1. Set @index of the previous black node,
 //   2. For all black nodes before it we set @index to @wi. This step I defer
 //      until the end, where I go back and set it.
-vector<Node> build_cascade(const vector<int> &w, const vector<Node> &b) {
+template<class T>
+// static
+void FCascade<T>::build_cascade(const vector<T> &w, const vector<Node> &b,
+      vector<Node> &res) {
   cout<<"w.size="<<w.size()<<" b.size="<<b.size()<<endl;
-  vector<Node> res(w.size() + (b.size() + 1) / 2);
+  res.resize(w.size() + (b.size() + 1) / 2);
   int wi = 0, bi = 0, ri = 0;
   int prev_black = -1;  // Previous cascaded node.
   for (; bi < b.size() && wi < w.size(); ri++) {
@@ -76,57 +82,66 @@ vector<Node> build_cascade(const vector<int> &w, const vector<Node> &b) {
       res[i].j = prev_white;
     }
   }
-  return res;
 }
 
 // Builds fixed fraction cascading (fraction parameter p = 1/2).
-FCascade build(const vecvec<int> &lists) {
-  FCascade fc;
-  fc.nodes.resize(lists.size());
-  fc.nodes[0] = build_first(lists[0]);
-  for (int i = 1; i < lists.size(); i++) {
-    fc.nodes[i] = build_cascade(lists[i], fc.nodes[i - 1]);
+template<class T>
+void FCascade<T>::add_list(const vector<T> &a) {
+  if (cascades_.empty()) {
+    cascades_.emplace_back(a.size());
+    auto &nl = cascades_.back();
+    for (int i = 0; i < a.size(); i++) {
+      nl[i].v = a[i];
+      nl[i].c = kWhite;
+      nl[i].j = nl[i].cascade_ix = -1;
+    }
+  } else {
+    auto &nl_prev = cascades_.back();
+    cascades_.emplace_back();
+    build_cascade(a, nl_prev, cascades_.back());
   }
-  return fc;
 }
 
 // Returns vector of list index (first) and index in the list (second) where
 // `n` where found.
-vector<pair<int, int>> search(const FCascade &fc, int n) {
+template<class T>
+vector<pair<int, int>> FCascade<T>::search(T x) const {
   vector<pair<int, int>> loc;
-  auto it = lower_bound(fc.nodes[0].begin(), fc.nodes[0].end(), n,
-        [](const Node& node, int n) {
-          return node.v < n;
-        });
-  int i0 = it == fc.nodes[0].end()
-      ? fc.nodes[0].size()
-      : it - fc.nodes[0].begin();
-  cout<<"i0="<<i0<<endl;
+  if (cascades_.empty()) return loc;
+
+  // int c = cascades_.size() - 1;
+  // auto it = lower_bound(cascades_[c].begin(), cascades_[c].end(), n,
+  //     [] (const Node& node, int n) {
+  //       return node.v < n;
+  //     });
+  // int i0 = it == cascades_[c].end()
+  //     ? cascades_[c].size()
+  //     : it - cascades_[c].begin();
+  // cout<<"i0="<<i0<<endl;
+  // if (cascades_.
   return loc;
 }
 
-void print(const FCascade &fc) {
+template<class T>
+void FCascade<T>::print() const {
   int l = 0;
-  for (auto &nodes : fc.nodes) {
-    cout<<"----NODES: level="<<(l++)<<endl;
-    for (int i = 0; i < nodes.size(); i++) {
-      auto &n = nodes[i];
+  for (auto &nl : cascades_) {
+    cout<<"----cascades_: level="<<(l++)<<endl;
+    for (int i = 0; i < nl.size(); i++) {
       cout<<"Node #"<<i
-          <<"\n  color="<<(n.c == kWhite ? "white" : "black")
-          <<"\n  v="<<n.v
-          <<"\n  j="<<n.j
-          <<"\n  cascade_ix="<<n.cascade_ix<<endl;
+          <<"\n  color="<<(nl[i].c == kWhite ? "white" : "black")
+          <<"\n  v="<<nl[i].v
+          <<"\n  j="<<nl[i].j
+          <<"\n  cascade_ix="<<nl[i].cascade_ix<<endl;
     }
   }
 }
 
 void test() {
-  vecvec<int> lists{
-    {1, 10, 20},
-    {5, 9, 15, 24},
-  };
-  auto fc = build(lists);
-  print(fc);
+  FCascade<int> fc;
+  fc.add_list({1, 10, 20});
+  fc.add_list({5, 9, 15, 24});
+  fc.print();
 }
 
 int main(int argc, char **argv) {

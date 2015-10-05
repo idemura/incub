97bedea74dbd8e4ba7a571c20bc9b502a063bbe7
Igor Demura <igor.demura@gmail.com>
2015-10-04 22:18:44 -0700

    iped

---
+++ iped/iped.cxx
@@ -0,0 +1,8 @@
+#include "iped.hxx"
+#include <iostream>
+
+int main(int arc, char **argv) {
+  std::cout<<"iped "<<kVersion<<std::endl;
+  return 0;
+}
+
---
+++ iped/iped.hxx
@@ -0,0 +1,7 @@
+#ifndef IPED_HXX_
+#define IPED_HXX_
+
+constexpr int kVersion = 1;
+
+#endif
+

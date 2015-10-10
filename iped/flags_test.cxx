66f9944cb877ea3a1d42da5a111720c23b4b2cae
Igor Demura <igor.demura@gmail.com>
2015-10-10 03:29:00 -0700

    tests

--- iped/flags_test.cxx
+++ iped/flags_test.cxx
@@ -12,7 +12,7 @@ class Flags: public FlagSet {
   double dbl = 0.5;
 
   Flags() {
-    register_flag("str", &);
+    register_flag("str", &str);
     register_flag("n32", &n32);
     register_flag("n64", &n64);
     register_flag("b", &b);
@@ -21,14 +21,28 @@ class Flags: public FlagSet {
 };
 
 void test1() {
-
+  char p0[] = "test_main";
+  char p1[] = "-n32";
+  char p2[] = "132";
+  char p3[] = "-str";
+  char p4[] = "xyz";
+  char* argv[] = {
+    p0, p1, p2, p3, p4, nullptr
+  };
+  Flags flags;
+  int argc = ARRAY_SIZEOF(argv) - 1;
+  CHECK(flags.parse(&argc, argv));
+  CHECK(argc == 1);
+  CHECK(flags.n32 == 132);
+  CHECK(flags.str == "xyz");
 }
-  
+
 }  // namespace
 }  // namespace
 
 int main() {
   using namespace iped::test;
+  std::ios_base::sync_with_stdio(false);
   test1();
   return 0;
 }

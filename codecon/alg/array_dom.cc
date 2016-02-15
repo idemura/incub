d8b4a9cd5a1322141b5229e4e5040c6d5b011b4d
Igor Demura <igor.demura@gmail.com>
2016-02-14 18:18:24 -0800

    array dominant

--- codecon/alg/array_dom.cc
+++ codecon/alg/array_dom.cc
@@ -1,7 +1,7 @@
 #include "base.h"
 
 // Given an array of integers. We know that one integer occurs more than n/2
-// times. Find this "dominant" element.
+// times. Find it.
 // Idea is that consecutive integers of the number we looking for happens more
 // than any others, even all together.
 // For simplicity, we assume integers are positive.

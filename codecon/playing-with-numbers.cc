4a67f8de8ba1d023f728ff22772e1e21332b706c
Igor Demura <igor.demura@thoughtspot.com>
2016-06-09 19:30:42 -0700

    playing with numbers

--- codecon/playing-with-numbers.cc
+++ codecon/playing-with-numbers.cc
@@ -3,67 +3,86 @@
 #include <vector>
 #include <string>
 #include <cstdio>
+#include <map>
 
 using namespace std;
 
 using i64 = long long int;
 
-int minor_bit(int n) {
-  return n & -n;
+struct Sum {
+  int s = 0, n = 0;
+  Sum() {}
+  Sum(int s, int n): s(s), n(n) {}
+};
+
+Sum operator-(Sum a, Sum b) {
+  a.s -= b.s;
+  a.n -= b.n;
+  return a;
+}
+Sum operator+(Sum a, Sum b) {
+  a.s += b.s;
+  a.n += b.n;
+  return a;
 }
 
-// Sum of @k elements we store at index (k - 1).
-vector<int> make_bit(vector<int> a) {
-  cout<<"input: ";
-  for (auto t : a) cout<<t<<" ";
-  cout<<endl;
-  for (int s = 2; s <= a.size(); s += s) {
-    cout<<"step "<<s<<endl;
-    for (int i = s - 1; i < a.size(); i += s) {
-      cout<<"adding to "<<i<<" elem at "<<(i - s/2)
-          <<": "<<a[i]<<" + "<<a[i - s/2]<<" = ";
-      a[i] += a[i - s / 2];
-      cout<<a[i]<<endl;
-    }
-    cout<<"bs: ";
-    for (auto g : a) cout<<g<<" ";
-    cout<<endl;
-  }
-  return move(a);  // Not sure. Probably NRVO doens't kick in.
-}
+struct SumMap {
+  map<int, Sum> m;
+  Sum total;
+};
 
-// @n - number of elements.
-int bit_sum(const vector<int> &b, int n) {
+SumMap sums(vector<int> &a) {
+  SumMap sm;
+  sort(a.begin(), a.end());
   int s = 0;
-  while (n != 0) {
-    s += b[n - 1];
-    n -= minor_bit(n);
+  for (int i = 0; i < a.size(); i++) {
+    sm.m[a[i]] = Sum(s, i);
+    s += a[i];
   }
-  return n;
+  sm.total = Sum(s, a.size());
+  return sm;
+}
+
+// os - opposite sign
+// ss - same sign
+int query(const SumMap &os, const SumMap &ss, int q) {
+  auto r = ss.total.s + ss.total.n * q;
+  auto lb = os.m.lower_bound(q);
+  if (lb == os.m.end()) {
+    r += q * os.total.n - os.total.s;
+  } else {
+    auto sum = lb->second;
+    r += q * sum.n - sum.s;  // 0 to lower bound
+    sum = os.total - sum;
+    r += sum.s - q * sum.n;  // lower bound till the end
+  }
+  return r;
 }
 
 int main() {
   int an = 0, qn = 0;
   scanf("%d", &an);
-  vector<int> a(an);
-  for (auto &x : a) scanf("%d", &x);
+  vector<int> neg, pos;
+  for (int i = 0; i < an; i++) {
+    int k = 0;
+    scanf("%d", &k);
+    if (k < 0)
+      neg.push_back(-k);
+    else
+      pos.push_back(k);
+  }
+  auto nsm = sums(neg);
+  auto psm = sums(pos);
   scanf("%d", &qn);
-  vector<int> q(qn);
-  for (auto &x : q) scanf("%d", &x);
-  sort(a.begin(), a.end());
-  auto bs = make_bit(a);
-  cout<<"bs: ";
-  for (auto g : bs) cout<<g<<" ";
-  cout<<endl;
-  auto positive = lower_bound(a.begin(), a.end(), 0);
-  auto s = 0;
-  for (auto x : q) {
-    s += x;
-    vector<int>::iterator i;
-    if (s < 0) {
-      i = lower_bound(positive, a.end(), -s);
+  int acc = 0;
+  for (int i = 0; i < qn; i++) {
+    int q = 0;
+    scanf("%d", &q);
+    acc += q;
+    if (acc < 0) {
+      printf("%d\n", query(nsm, psm, -acc));
     } else {
-      i = lower_bound(a.begin(), positive, -s);
+      printf("%d\n", query(psm, nsm, acc));
     }
   }
   return 0;

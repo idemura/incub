445c7e30c1eca9622cb883e870e98aade811f1b8
Igor Demura <igor.demura@gmail.com>
2016-06-23 11:31:38 -0700

    type spec grammar

--- demur/parser_test.cxx
+++ demur/parser_test.cxx
@@ -27,12 +27,57 @@ void test_fn() {
       "fn foo(n: Int) {}\n"
       "fn bar(x, y, z: Int) {}\n"
       "fn bar(x, y: Int, s: String) {}\n"
+      "fn foo(): Int {}\n"
+      "fn foo(x: Int): Int {}\n"
+      "fn foo(x: Int): x: Int {}\n"
+      "fn foo(x: Int): x, y: Int {}\n"
+      "fn foo(x: Int): x: Int, y: Int {}\n"
+      "fn foo(x: Int): Int, Int {}\n"
+  ));
+  // This kind of arg list is accepted by grammar, but possibly should be
+  // rejected by semantic analyzer.
+  CHECK(0 == parse_test(
+      "fn foo(Int) {}\n"
+      "fn foo(Int, Int) {}\n"
+  ));
+  CHECK(1 == parse_test(
+      "fn foo(: Int) {}\n"
   ));
   CHECK(1 == parse_test(
       "fn foo(n: Int,) {}\n"
   ));
   CHECK(1 == parse_test(
       "fn foo(n,: Int) {}\n"
+  ));
+  CHECK(1 == parse_test(
+      "fn foo(n: int) {}\n"
+  ));
+  CHECK(1 == parse_test(
+      "fn foo(n:) {}\n"
+  ));
+  CHECK(1 == parse_test(
+      "fn foo(n: Int): {}\n"
+  ));
+}
+
+void test_type_spec() {
+  // Use that function grammar accepts just type_spec.
+  CHECK(0 == parse_test(
+      "fn foo(T) {}\n"
+      "fn foo((T)) {}\n"
+      "fn foo([T, T]) {}\n"
+      "fn foo(([T, T])) {}\n"
+      "fn foo(T => T) {}\n"
+      "fn foo(T[]) {}\n"
+      "fn foo(T => T[]) {}\n"
+      "fn foo(T[] => T) {}\n"
+      "fn foo([T, T] => T[]) {}\n"
+      "fn foo(T => [T, T] => T[]) {}\n"
+      "fn foo(T => (T => T)[] => (T => [T, T])) {}\n"
+      "# big sample:\n"
+      "fn foo(f: T => (T => T)[] => (T => [T, T]), n: T):\n"
+      "    [T, T, T]\n {"
+      "}\n"
   ));
 }
 
@@ -48,6 +93,7 @@ int main(int argc, char **argv) {
 
   test_comment();
   test_fn();
+  test_type_spec();
 
   flags_reset();
   RETURN_TESTS_PASSED();

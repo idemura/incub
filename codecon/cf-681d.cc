0c9638f9a76ab59dfade2c123bc502b2de51242e
Igor Demura <igor.demura@thoughtspot.com>
2016-06-30 16:53:47 -0700

    cf 681d

--- codecon/cf-681d.cc
+++ codecon/cf-681d.cc
@@ -19,40 +19,56 @@ using u64 = unsigned i64;
 using i32 = int;
 using u32 = unsigned i32;
 
-constexpr int INF = 0x7fffffff;
-constexpr int MOD = 1000000007;
+using AdjL = vector<vector<int>>;
 
-struct Node {
-  int children = 0;
-  int to = -1, from = -1;
-  Node *p = nullptr;
-
-  Node() = default;
-};
+// Every node n in a subtree of a @node with node == node->to should have
+// n->to == node.
+bool dfs(const AdjL &al,
+         const vector<int> &ancestor,
+         int v,
+         int root,
+         vector<int> *res) {
+  if (ancestor[v] == v) root = v;
+  if (ancestor[v] != root) {
+    return false;
+  }
+  for (auto u : al[v]) {
+    if (!dfs(al, ancestor, u, root, res)) return false;
+  }
+  // Put them in list in post order.
+  if (ancestor[v] == v) {
+    res->push_back(v + 1);
+  }
+  return true;
+}
 
 int main(int argc, char **argv) {
-  int n, m, p, q, a;
+  int n, m, p, q;
   cin>>n>>m;
-  vector<Node> nodes(n);
-  for (int i = 0; i < n; i++) {
-    nodes[i].n = i;
-  }
+  AdjL al(n);
+  vector<int> ancestor(n), has_parent(n);
   for (int i = 0; i < m; i++) {
     cin>>p>>q;
-    nodes[q - 1].parent = &nodes[p - 1];
-    nodes[p - 1].children++;
+    al[p - 1].push_back(q - 1);
+    has_parent[q - 1] = 1;
   }
   for (int i = 0; i < n; i++) {
-    cin>>a;
-    nodes[i].to = a - 1;
-    nodes[a - 1].from = i;
+    cin>>p;
+    ancestor[i] = p - 1;
   }
-  vector<Node*> leaf;
-  for (auto &n : nodes) {
-    if (n.children == 0) leaf.push_back(&n);
+  vector<int> res;
+  for (int i = 0; i < n; i++) {
+    if (has_parent[i]) continue;
+    if (!dfs(al, ancestor, i, -1, &res)) {
+      res.clear();  // Clear list is never a valid.
+      break;
+    }
   }
-  for (int i = 0; i < leaf; i++) {
+  if (res.empty()) {
+    cout<<"-1\n";
+  } else {
+    cout<<res.size()<<"\n";
+    for (auto k : res) cout<<k<<"\n";
   }
   return 0;
 }
-

523ce4ae951b24ee6e5eec61d63e5afcaa4fee83
Igor Demura <igor.demura@thoughtspot.com>
2016-06-30 16:58:27 -0700

    cf 687b

---
+++ codecon/cf-687b.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long int;
+using u64 = unsigned i64;
+using i32 = int;
+using u32 = unsigned i32;
+
+constexpr int INF = 0x7fffffff;
+constexpr int MOD = 1000000007;
+
+int main(int argc, char **argv) {
+  return 0;
+}
+

62c5337bd50076bb2e86e11502c482985105489d
Igor Demura <igor.demura@gmail.com>
2016-07-08 09:47:10 -0700

    cf-687b

--- codecon/cf-687b.cc
+++ codecon/cf-687b.cc
@@ -7,34 +7,11 @@ using u64 = unsigned i64;
 using i32 = int;
 using u32 = unsigned i32;
 
-constexpr int INF = 0x7fffffff;
-constexpr int MOD = 1000000007;
-
-// From Chinese remainder theorem, we need b = c[1]*...*c[k-1]*c[k+1]*...*c[n]
-// to be coprime with c[k] to be able to invert b. This means that c[1]...c[n]
-// are have to be coprime.
-// Additionally, k from the input that we should be less than product of all
-// the c[k].
-
-bool prod_gt(const vector<int> &a, int k) {
-  i64 p = 1;
-  for (auto m : a) {
-    p *= m;
-    if (p >= k) return false;
-  }
-  cout<<"prod true"<<endl;
-  return true;
-}
-
-int sqrt_int(int n) {
-  return int(sqrt(n));
-}
-
 // Result[i] means minimal divisor of i. Use it in `factorize`.
 vector<int> sieve(int n) {
   vector<int> factor(n + 1);
   factor[1] = 1;
-  auto i = 2, imax = sqrt_int(n);
+  auto i = 2, imax = (int)sqrt(n);
   for (; i <= imax; i++) {
     if (factor[i] != 0) continue;
     factor[i] = i;
@@ -50,30 +27,59 @@ vector<int> sieve(int n) {
   return factor;
 }
 
-bool coprime(const vector<int> &a, int a_max) {
-  auto s = sieve(a_max);
-  // Factoring any number of a[i] should not intersect.
-  vector<int> factor_used(a_max);
-  for (auto k : a) {
-    cout<<"factor "<<k<<endl;
-    while (k != 1) {
-      cout<<"k "<<k<<endl;
-      cout<<"divisor "<<s[k]<<endl;
-      if (factor_used[s[k]]) return false;
-      factor_used[s[k]] = 1;
-      k /= s[k];
+bool can_find_remainder() {
+  int n, k;
+  cin>>n>>k;
+  vector<int> c(n);
+  auto c_max = 0;
+  for (auto &m : c) {
+    cin>>m;
+    if (m > c_max) c_max = m;
+  }
+  if (k == 1) return true;
+  const auto sv = sieve(c_max);
+  // LCM of @c in form of a map factor=>degree.
+  unordered_map<int, int> lcm;
+  for (auto n : c) {
+    while (n != 1) {
+      auto f = sv[n];
+      auto d = 0;
+      do {
+        n /= f;
+        d++;
+      } while (n % f == 0);
+      const auto i = lcm.find(f);
+      if (i == lcm.end()) {
+        lcm[f] = d;
+      } else if (i->second < d) {
+        i->second = d;
+      }
     }
   }
-  cout<<"coprime true"<<endl;
+  // First, check LCM is no less than k. After check LCM divisable by k. Later
+  // check modifies lcm and k.
+  i64 p = 1;
+  for (auto fd : lcm) {
+    for (int i = 0; i < fd.second && p < k; i++) {
+      p *= fd.first;
+    }
+    if (!(p < k)) break;
+  }
+  if (p < k) return false;
+
+  // Check if divisale by k.
+  for (int k1 = k; k1 != 1;) {
+    const auto i = lcm.find(sv[k1]);
+    if (i == lcm.end()) return false;
+    if (i->second == 0) return false;
+    i->second--;
+    k1 /= sv[k1];
+  }
   return true;
 }
 
 int main() {
-  int n, k;
-  cin>>n>>k;
-  vector<int> c(n);
-  for (auto &m : c) cin>>m;
-  if (coprime(c, 1'000'004) && prod_gt(c, k)) {
+  if (can_find_remainder()) {
     cout<<"Yes\n";
   } else {
     cout<<"No\n";

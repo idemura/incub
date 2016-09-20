aba0d58544c169e6abe41bb11ba627d0e9a41082
Igor Demura <igor.demura@gmail.com>
2016-09-19 21:39:36 -0700

    parser build

--- jowl/src/owl/lang/Main.java
+++ jowl/src/owl/lang/Main.java
@@ -1,7 +1,11 @@
 package owl.lang;
 
+import owl.lang.Token;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("hello");
+    Token t = new Token(10);
+    System.out.println(t.toString());
   }
 }

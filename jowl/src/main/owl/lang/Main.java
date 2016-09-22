bf5c87413a6096e6cd566467fa0ac73d24a983ab
Igor Demura <igor.demura@gmail.com>
2016-09-21 21:40:32 -0700

    sum parser in javacc

--- jowl/src/main/owl/lang/Main.java
+++ jowl/src/main/owl/lang/Main.java
@@ -1,10 +1,49 @@
 package owl.lang;
 
+import java.io.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello");
-    SimpleCharStream reader = new SimpleCharStream(System.in);
-    Parser parser = new Parser(new ParserTokenManager(reader));
-    System.out.println("new version");
+    boolean failed = false;
+    for (String file_name : args) {
+      if (!compileFile(file_name)) {
+        failed = true;
+      }
+    }
+    System.exit(failed? 1: 0);
+  }
+
+  private static boolean compileFile(String file_name) {
+    InputStream in = null;
+    try {
+      in = new FileInputStream(new File(file_name));
+      return compile(in);
+    } catch (IOException e) {
+      System.err.println("File error: " + e.getMessage());
+      return false;
+    } finally {
+      closeStream(in);
+    }
+  }
+
+  private static boolean compile(InputStream in) {
+    Parser parser = new Parser(new ParserTokenManager(new SimpleCharStream(in)));
+    try {
+      System.out.println(parser.number());
+    } catch (ParseException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+
+  private static void closeStream(InputStream in) {
+    if (in != null) {
+      try {
+        in.close();
+      } catch (IOException e) {
+        // Empty
+      }
+    }
   }
 }

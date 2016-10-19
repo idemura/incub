64b0cfff7833bc8f3c34b97b489c2c3ea817d5b9
Igor Demura <igor.demura@gmail.com>
2016-10-18 20:58:33 -0700

    push relabel with q

--- codecon/push_relabel.cc
+++ codecon/push_relabel.cc
@@ -3,6 +3,7 @@
 using namespace std;
 
 bool FLAG_step_mode = false;
+
 /*
 Seems like relabel happens until vertex has no excess or all edges saturated,
 because it only goes up.
@@ -11,8 +12,12 @@ because it only goes up.
 void print_matrix(const vector<vector<int>> &m, const string &tag) {
     cout<<tag<<":\n";
     for (const auto &r : m) {
-        for (auto x : r) {
-            cout<<x<<" ";
+        if (r.empty()) {
+            cout<<"<empty>";
+        } else {
+            for (auto x : r) {
+                cout<<x<<" ";
+            }
         }
         cout<<"\n";
     }
@@ -28,6 +33,7 @@ void print_vector(const vector<int> &r, const string &tag) {
 
 void wait_enter() {
     if (FLAG_step_mode) {
+        cout<<"Press Enter... ";
         string s;
         getline(cin, s);
     }
@@ -37,6 +43,78 @@ vector<vector<int>> make_matrix(int n) {
     vector<vector<int>> m(n);
     for (auto &r : m) r.resize(m.size());
     return m;
+}
+
+// Allowed to push of height (h - 1) where h is the height of the current
+// top.
+class PushRelabelQ {
+  public:
+    explicit PushRelabelQ(int h_max)
+            : q(h_max),
+              not_empty(h_max) {
+    }
+
+    void init(int v) {
+        q[0].push_back(v);
+    }
+
+    int get_top() {
+        return q[top].back();
+    }
+
+    // Relabels top.
+    void relabel() {
+        auto v = q[top].back();
+        q[top].pop_back();
+        if (q[top].empty()) {
+            not_empty[top + 1] = not_empty[top];
+        } else {
+            not_empty[top + 1] = top;
+        }
+        top++;
+        q[top].push_back(v);
+    }
+
+    // Do this before @pop.
+    void add_downhill(int v) {
+        q[top - 1].push_back(v);
+    }
+
+    void pop() {
+        q[top].pop_back();
+        if (q[top].empty()) {
+            top = not_empty[top];
+        }
+    }
+
+    bool empty() const {
+        return top == 0 && q[top].empty();
+    }
+
+    void print(ostream &os) const {
+        os<<"PushRelabelQ ("<<q.size()<<" level(s)):\n";
+        for (int i = 0; i < q.size(); i++) {
+            cout<<"  h="<<i<<" not_empty="<<not_empty[i]<<": ";
+            if (q[i].empty()) {
+                cout<<"<empty>";
+            } else {
+                for (auto x : q[i]) {
+                    cout<<x<<" ";
+                }
+            }
+            cout<<"\n";
+        }
+    }
+
+  private:
+    int top = 0;
+    vector<vector<int>> q;
+    vector<int> not_empty;
+};
+
+ostream &operator<<(ostream &os, const PushRelabelQ &q) {
+    q.print(os);
+    return os;
 }
 
 // @c_in is matrix of capacities.
@@ -50,10 +128,15 @@ vector<vector<int>> push_relabel(
     const auto n = c.size();
     auto f = make_matrix(n);
     // We can undo flow excess back where it came from.
-    vector<vector<int>> h(2 * n);
-    int h_top = 0;
+    //vector<vector<int>> h(2 * n);
+    // Height of each vertex.
+    vector<int> h(n);
+    h[s] = n;
+    // Queue of vertices with excess by height. Highest - last.
+    PushRelabelQ q(2 * n);
+    //int h_top = 0;
     // Maps height to previous height.
-    vector<int> prev(2 * n, -1);
+    //vector<int> prev(2 * n, -1);
     //h[s] = n;
     vector<int> e(n);
     cout<<"__ 1a"<<endl;
@@ -66,89 +149,83 @@ vector<vector<int>> push_relabel(
             //c[i][s] += f[s][i];    // There can be an arc back already.
             //b[i][s] += f[s][i];
             c[s][i] = 0;
-            h[0].push_back(i);
+            q.init(i);
         }
     }
     //print_matrix(b, "initial b");
 
     // cout<<"__ 1"<<endl;
-    for (;;) {
+    while (!q.empty()) {
         wait_enter();
         cout<<endl;
-        if (h_top < 0) {
-            cout<<"flow done"<<endl;
-            break;
-        }
-        cout<<"h_top="<<h_top<<endl;
-        auto v = h[h_top].back();
-        print_vector(h[h_top], "h_top list");
-        cout<<"pick vertex v="<<endl;
-        h[h_top].pop_back();
-        if (h[h_top].empty()) {
-            print_vector(prev, "goto prev");
-            h_top = prev[h_top];
-            cout<<"new h_top="<<h_top<<endl;
-        }
+        print_vector(e, "excess");
+        //cout<<"h_top="<<h_top<<endl;
+        auto v = q.get_top();
+        cout<<q;
+        print_vector(h, "h");
+        cout<<"pick vertex v="<<v<< endl;
         print_vector(downhill[v], "downhill");
         if (downhill[v].empty()) {
             // Relabel
-            cout<<"relabel "<<v<<endl;
-            h_top++;
-            cout<<"increase h_top"<<h_top<<endl;
-            h[h_top].push_back(v);
-            print_vector(h[h_top], "h_top new");
-            prev[h_top] = prev[h_top - 1];
-            print_vector(h[h_top - 1], "check for new downhill in");
-            for (int j = 0; j < h[h_top - 1].size(); j++) {
-                if (c[v][j] + f[j][v] > 0) {
-                    cout<<"new flow "<<(c[v][j] + f[j][v])<<endl;
+            cout<<"relabel v="<<v<<endl;
+            q.relabel();
+            h[v]++;
+            cout<<"new h="<<h[v]<<endl;
+            downhill[v].clear();
+            for (int j = 0; j < n; j++) {
+                if (h[j] < h[v] && c[v][j] + f[j][v] > 0) {
+                    cout<<"added "<<j<<endl;
                     downhill[v].push_back(j);
+                } else {
+                    cout<<j<<" is not downhill: "<<h[j]<<endl;
                 }
             }
+            cout<<"end downhill"<<endl;
             //h[v]++;
-            print_matrix(h, "h");
+            print_vector(h, "h");
+            //q.push_back(v);
+            //q.add_front(v);
         } else {
-            cout<<"push"<<endl;
+            //cout<<"push"<<endl;
             // Push
             auto w = downhill[v].back();
             cout<<"push to w="<<w<<endl;
             downhill[v].pop_back();
             // Cancel some flow to the vertex and use new capacity.
             //auto d = min(c[v][w] + b[v][w], e[v]);
-            cout<<"flow to my v from w: "<<f[w][v]<<endl;
-            if (e[v] < f[w][v]) {
-                cout<<"flow to me is more than excess\n";
-                f[w][v] -= e[v];
-                c[w][v] += e[v];
-                e[w] += e[v];
-                e[v] = 0;
-                cout<<"clear downhill"<<endl;
-                downhill[v].clear();
-            } else {
-                cout<<"need to use capacity\n";
-                auto d = min(e[v], c[v][w] + f[w][v]);
-                if (e[v] > d) {
-                    cout<<"excess is not empty, return v"<<endl;
-                    h[h_top].push_back(v);
-                } else {
-                    //downhill[v].push_back(w);
-                    // clear downhill actually
-                    cout<<"excess is 0"<<endl;
-                    downhill[v].clear();
-                }
-                cout<<"d to move: "<<d<<endl;
+            cout<<"back flow "<<w<<" to "<<v<<": "<<f[w][v]<<endl;
+            auto d = min(e[v], f[w][v]);
+            cout<<"d="<<d<<" e[v]="<<e[v]<<" c+f="<<(c[v][w] + f[w][v])<<endl;
+            if (e[w] == 0) {
+                q.add_downhill(w);
+            }
+            //auto no_excess_w = e[w] == 0;
+            e[w] += d;
+            e[v] -= d;
+            c[w][v] += d;
+            f[w][v] -= d;
+            if (e[v] > 0) {
+                d = min(e[v], c[v][w]);
+                cout<<"need to use capacity d="<<d<<endl;
                 e[w] += d;
                 e[v] -= d;
-                d -= f[w][v];
                 c[v][w] -= d;
                 f[v][w] += d;
-                c[w][v] += f[w][v];
-                f[w][v] = 0;
+            }
+            if (e[v] == 0) {
+                cout<<"downhill cleared"<<endl;
+                downhill[v].clear();
+                q.pop();
+            } else {
+                cout<<"leave in q"<<endl;
+                //q.add_front(v);
+                //q.push_back(v);
             }
             // print_matrix(c, "c");
             // print_matrix(f, "f");
             // print_vector(e, "e");
         }
+        cout<<"new q:\n"<<q;
     }
     return f;
 }
@@ -381,7 +458,7 @@ void test4() {
 }
 
 int main() {
-    FLAG_step_mode = false;
+    FLAG_step_mode = true;
     test1();
     test2();
     test3();

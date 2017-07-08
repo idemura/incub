bca4e9d6da0fbedb87d1ef5a5a4ec14c17a371c9
Igor Demura <igor.demura@gmail.com>
2017-07-07 18:22:16 -0700

    copy/make sln

--- codecon/copy_sln.sh
+++ codecon/copy_sln.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
-if [ "$#" -ne 1 ]; then
-  exit -1
-fi
+[ "$#" -eq 1 ] || exit 1
 
 if cp "$1".cc "sln/"; then
   rm -r "$1"*
   git add sln/"$1".cc
   echo "$1 copied and added"
 fi
-
---
+++ codecon/make_sln.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+[ "$#" -eq 1 ] || exit 1
+
+cat > "$1".cc <<EOF
+#include <array>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <iostream>
+
+#include "logger.h"
+
+using namespace std;
+
+using i64 = long long int;
+using pii = std::pair<int, int>;
+
+int main() {
+
+    return 0;
+}
+EOF
+
+touch "$1".in
+
+echo "./build.sh $1.cc && ./$1 < $1.in" | pbcopy
+echo "Build and run command line is copied"

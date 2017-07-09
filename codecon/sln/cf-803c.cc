01cf39bb4df36ca2eaf3442fbc5fce527232addd
Igor Demura <igor.demura@gmail.com>
2017-07-09 11:19:46 -0700

    cf-803c

---
+++ codecon/sln/cf-803c.cc
@@ -0,0 +1,54 @@
+#include <cmath>
+#include <array>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <iostream>
+
+using namespace std;
+
+using i64 = long long int;
+using pii = std::pair<int, int>;
+
+namespace {
+i64 n = 0, k = 0;
+i64 gcd = 0;
+i64 ap_rem = 0;
+}
+
+void check(i64 divisor, i64 n1) {
+    // Estimation on max k we can have with AP:
+    auto k1 = (int)sqrt(2 * n1);
+    if (k <= k1) {
+        auto ap = k * (k - 1) / 2;  // sum(1, 2, ... (k-1))
+        if (ap <= n1 && n1 - ap >= k && divisor > gcd) {
+            gcd = divisor;
+            ap_rem = n1 - ap;
+        }
+    }
+}
+
+int main() {
+    cin>>n>>k;
+    for (i64 i = 1, j = n; i <= j;) {
+        if (n % i == 0) {
+            auto n1 = n / i;
+            check(i, n1);
+            check(n1, i);
+        }
+        i++;
+        j = n / i;
+    }
+    if (gcd == 0) {
+        printf("-1\n");
+    } else {
+        for (int i = 1; i < k; i++) {
+            cout<<gcd * i<<" ";
+        }
+        cout<<gcd * ap_rem<<"\n";
+    }
+    return 0;
+}

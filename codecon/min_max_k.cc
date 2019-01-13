180bd838fc448b0d0ea5e43691c0aae7942da37b
Igor Demura <igor.demura@gmail.com>
2019-01-12 19:20:49 -0800

    min/max k

--- codecon/min_max_k.cc
+++ codecon/min_max_k.cc
@@ -4,23 +4,56 @@
 #include <algorithm>
 #include <utility>
 #include <vector>
+#include <deque>
+#include <unordered_map>
+#include <string>
 
 #include "log.h"
 #include <gtest/gtest.h>
 
 using namespace std;
 
-pair<int, int> findMinMaxK(vector<int> const &a, int k) {
-    pair<int, int> res{0, a.size()};
-    vector<int> stack;
-    stack.push_back(0);
-    for (int i = 1; i < a.size(); i++) {
+struct ClosestRightState {
+    unordered_map<int, vector<int>> invIdx;
+
+    explicit ClosestRightState(vector<int> const &a) {
+        assert(invIdx.empty());
+        for (int i = 0; i < a.size(); i++) {
+            invIdx[a[i]].push_back(i);
+        }
     }
-    return res;
+
+    int findToRight(int x, int from) const {
+        auto itr = invIdx.find(x);
+        if (itr == invIdx.end()) {
+            return -1;
+        }
+        auto const &pl = itr->second;
+        string s;
+        for (auto a : pl) {
+            s.append(to_string(a)).append(" ");
+        }
+        auto pos = lower_bound(pl.begin(), pl.end(), from);
+        if (pos == pl.end()) {
+            return -1;
+        }
+        return *pos;
+    }
+};
+
+pair<int, int> findMinMaxK(vector<int> const &a, int k) {
+    return {0, a.size()};
 }
 
 TEST(MinMaxK, Case1) {
-    auto res = findMinMaxK(vector<int>{1, 2, 6, 3, 5, 7}, 3);
+    vector<int> a{1, 2, 6, 3, 2, 5, 7};
+    ClosestRightState crs{a};
+    EXPECT_EQ(1, crs.findToRight(2, 0));
+    EXPECT_EQ(1, crs.findToRight(2, 1));
+    EXPECT_EQ(4, crs.findToRight(2, 2));
+    EXPECT_EQ(4, crs.findToRight(2, 3));
+    EXPECT_EQ(4, crs.findToRight(2, 4));
+    EXPECT_EQ(-1, crs.findToRight(2, 5));
 }
 
 int main(int argc, char **argv) {
